2960f1708d615466074c1af5d511197ddcc8bc19
wsmarshall <42559252+wsmarshall@users.noreply.github.com>
2023-11-24 13:59:21 -0800

    iterates on attempts to address type errors

--- twosum.rs
+++ twosum.rs
@@ -3,14 +3,19 @@ use std::collections::HashMap;
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         let mut entries = HashMap::new();
-        let length = nums.len() as i32;
+        let length = nums.len();
+        let result: Vec<i32> = Vec::new();  
 
         for i in 0..length {
-            let complement: i32 = target - *nums.get(i);
+            let complement: i32 = target - (nums.get(i) as i32);
             if entries.contains_key(&complement) {
-                return vec![i.try_into().unwrap(), entries.get(&complement)];
+                result.push(i as i32);
+                result.push(entries.get(&complement) as i32);
+                break;
             }
-            entries.insert(nums.get(i),i);
+            entries.insert(nums.get(i) as i32,i as i32);
         } 
+
+        result
     }
 }
