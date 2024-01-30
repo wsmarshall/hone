2522714da397fe313983434fcbeefcfa84d8fc8a
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-01-30 14:48:00 +0800

    sketches out anagram checker

---
+++ ntcd/ji6sei3ji6.rs
@@ -0,0 +1,7 @@
+impl Solution {
+    pub fn is_anagram(s: String, t: String) -> bool {
+        //put each of them into a separate hashmap
+
+        //then compare the two hashmaps for equality
+    }
+}
