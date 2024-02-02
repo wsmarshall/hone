68ac5420246bb2e2b5c09dd6bf502600da41b282
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-02-02 14:18:31 +0800

    implements ls approach for checking anagrams

--- ntcd/ji6sei3ji6.rs
+++ ntcd/ji6sei3ji6.rs
@@ -1,7 +1,32 @@
+use std::collections::HashMap;
+
 impl Solution {
     pub fn is_anagram(s: String, t: String) -> bool {
-        //put each of them into a separate hashmap
+        let mut s_map = HashMap::<char, i32>::new();
+        let mut t_map = HashMap::<char, i32>::new();
 
-        //then compare the two hashmaps for equality
+        for char in s.chars() {
+            if (s_map.contains_key(&char)) {
+                let new_count = add_one(s_map.get(&char));
+                s_map.insert(char, new_count);
+            } else {
+                s_map.insert(char, 1);
+            }
+        }
+
+        for char in t.chars() {
+            if (t_map.contains_key(&char)) {
+                let new_count = add_one(t_map.get(&char));
+                t_map.insert(char, new_count);
+            } else {
+                t_map.insert(char, 1);
+            }
+        }
+
+        return s_map == t_map;
     }
+}
+
+fn add_one(x: Option<&i32>) -> i32 {
+    *x.unwrap_or(&0) + 1
 }
