ccae156917cf370938bd33c6ac75666254e3fb3d
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-02-04 16:07:45 +0800

    implements first pass at validPal

--- ntcd/02tp/jat1ji6ng5.rs
+++ ntcd/02tp/jat1ji6ng5.rs
@@ -1,3 +1,22 @@
 impl Solution {
-    pub fn is_palindrome(s: String) -> bool {}
+    pub fn is_palindrome(s: String) -> bool {
+        //base case of empty string
+        if (s == " " || s == "") {
+            return true;
+        }
+        let input: String = s.chars().filter(|a| a.is_alphabetic()).collect();
+        let check: String = input.to_lowercase();
+
+        let mut start = 0;
+        let mut end = check.len() - 1;
+
+        while (start <= end) {
+            if (check.chars().nth(start) != check.chars().nth(end)) {
+                return false;
+            }
+            start = start + 1;
+            end = end - 1;
+        }
+        return true;
+    }
 }
