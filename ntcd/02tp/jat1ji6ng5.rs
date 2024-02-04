567714e49876f617ee4d9e092732ca0d39e2ea8a
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-02-04 17:27:21 +0800

    starts a first pass at ASCII/pointer implementation

--- ntcd/02tp/jat1ji6ng5.rs
+++ ntcd/02tp/jat1ji6ng5.rs
@@ -1,22 +1,18 @@
 impl Solution {
     pub fn is_palindrome(s: String) -> bool {
-        //base case of empty string
-        if (s == " " || s == "") {
-            return true;
-        }
-        let input: String = s.chars().filter(|a| a.is_alphanumeric()).collect();
-        let check: String = input.to_lowercase();
+        let mut left = 0;
+        let mut right = s.len() - 1;
 
-        let mut start = 0;
-        let mut end = check.len() - 1;
-
-        while (start <= end) {
-            if (check.chars().nth(start) != check.chars().nth(end)) {
-                return false;
-            }
-            start = start + 1;
-            end = end - 1;
+        while (left <= right) {
+            left += 1;
+            right -= 1;
         }
         return true;
+    }
+
+    fn an_check(c: char) {
+        return (((48 <= c.as_ascii()) && (c.as_ascii() <= 57))
+            || ((65 <= c) && (c <= 90))
+            || ((97 <= c) && (c <= 122)));
     }
 }
