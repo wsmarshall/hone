e5bf703ac8af5cf4e5d9f1466645955d839aebbc
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-02-05 14:57:57 +0800

    updates with current (buggy) palDetect

--- ntcd/02tp/jat1ji6ng5.rs
+++ ntcd/02tp/jat1ji6ng5.rs
@@ -1,22 +1,38 @@
+extern crate palindrome;
+
 impl Solution {
     pub fn is_palindrome(s: String) -> bool {
-        //base case of empty string
-        if (s == " " || s == "") {
-            return true;
-        }
-        let input: String = s.chars().filter(|a| a.is_alphanumeric()).collect();
-        let check: String = input.to_lowercase();
+        palindrome::is_palindrome(s)
+        let mut left = 0; 
+        let length = s.chars().count();
+        println!("length: {}", length);
+        let mut right = length -1;
 
-        let mut start = 0;
-        let mut end = check.len() - 1;
-
-        while (start < end && end < check.len()) {
-            if (check.chars().nth(start) != check.chars().nth(end)) {
+        while (left < right && right < length) {
+            println!("left, right: {}, {}", left, right);
+            while(!Self::an_check(s.as_bytes()[left]) && left < right){
+                left += 1;
+            }
+            while(!Self::an_check(s.as_bytes()[right]) && right < length){
+                right -= 1;
+            }
+            println!("left, right: {}, {}", left, right);
+            if( (s.to_lowercase().chars().nth(left)) != 
+            (s.to_lowercase().chars().nth(right))){
                 return false;
             }
-            start = start + 1;
-            end = end - 1;
+            left += 1;
+            right -= 1;
         }
         return true;
+
+    }
+
+    fn an_check(c: u8) -> bool {
+        println!("c: {}", c);
+        
+        (((48 <= c) && (c <= 57))||
+        ((65 <= c) && (c <= 90))||
+        ((97 <= c) && (c <= 122)))
     }
 }
