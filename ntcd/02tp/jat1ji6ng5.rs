0f9540a0663a923c0395d5086d6e168d188e58df
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-02-05 14:41:29 +0800

    adds working palDetect

--- ntcd/02tp/jat1ji6ng5.rs
+++ ntcd/02tp/jat1ji6ng5.rs
@@ -1,18 +1,22 @@
 impl Solution {
     pub fn is_palindrome(s: String) -> bool {
-        let mut left = 0;
-        let mut right = s.len() - 1;
+        //base case of empty string
+        if (s == " " || s == "") {
+            return true;
+        }
+        let input: String = s.chars().filter(|a| a.is_alphanumeric()).collect();
+        let check: String = input.to_lowercase();
 
-        while (left <= right) {
-            left += 1;
-            right -= 1;
+        let mut start = 0;
+        let mut end = check.len() - 1;
+
+        while (start < end && end < check.len()) {
+            if (check.chars().nth(start) != check.chars().nth(end)) {
+                return false;
+            }
+            start = start + 1;
+            end = end - 1;
         }
         return true;
-    }
-
-    fn an_check(c: char) {
-        return (((48 <= c.as_ascii()) && (c.as_ascii() <= 57))
-            || ((65 <= c) && (c <= 90))
-            || ((97 <= c) && (c <= 122)));
     }
 }
