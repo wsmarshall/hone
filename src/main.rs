0f1b04f53656fca8ed91fbff0f89f571ea84376e
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-03-22 15:08:42 -0700

    adds inner match for better dif btwn  file creation and opening errors

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,14 @@ fn main() {
 
     let greeting_file = match greeting_file_result {
         Ok(file) => file,
-        Err(error) => panic!("Problem opening the file: {:?}", error),
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error);
+            }
+        },
     };
 }
