beb48731b5b9c33a40b081e0ad3b4ac1dded10a9
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-04-01 16:04:16 -0700

    adds largest fn for chars

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,18 @@ fn largest(list: &[i32]) -> &i32 {
     largest
 }
 
+fn largest_char(list: &[char]) -> &char {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
