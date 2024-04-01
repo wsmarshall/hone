14a8bd4aaad003c2dd149f4e6bb133971ce440f6
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-04-01 16:06:24 -0700

    fully separates char and i32 fns before generic merging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 
-fn largest(list: &[i32]) -> &i32 {
+fn largest_i32(list: &[i32]) -> &i32 {
     let mut largest = &list[0];
 
     for item in list {
@@ -27,11 +27,11 @@ fn largest_char(list: &[char]) -> &char {
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest(&number_list);
+    let result = largest_i32(&number_list);
     println!("The largest number is {}", result);
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest(&number_list);
+    let result = largest_char(&char_list);
     println!("The largest number is {}", result);
 }
