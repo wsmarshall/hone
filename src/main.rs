8e22d03b82098f98fbd0b280810568b8cb58da78
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-04-02 14:59:07 -0700

    adds test driver for tweet creation, summarization

--- src/main.rs
+++ src/main.rs
@@ -28,4 +28,13 @@ impl Summary for Tweet {
     }
 }
 
-fn main() {}
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+}
