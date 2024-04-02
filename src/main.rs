1ef3e0dba755f9b0bddeab0e5f6d32bf41687371
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-04-02 14:55:47 -0700

    first implementation on traits

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,31 @@
-use std::fs::File;
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
 
-fn largest<T>(list: &[T]) -> &T {
-    let mut largest = &list[0];
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
 
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
     }
-
-    largest
 }
 
-fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest(&char_list);
-    println!("The largest number is {}", result);
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
 }
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+fn main() {}
