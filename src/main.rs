2692aa662a6e3cc5f1c001048dc3e60223e7cb2c
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-04-03 17:44:24 -0700

    starts reference lifetime examples

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,7 @@
-pub trait Summary {
-    fn summarize(&self) -> String {
-        String::from("[Default string from Summary Trait]")
-    }
-}
-
-pub struct NewsArticle {
-    pub headline: String,
-    pub location: String,
-    pub author: String,
-    pub content: String,
-}
-
-impl Summary for NewsArticle {
-    fn summarize(&self) -> String {
-        format!("{}, by {} ({})", self.headline, self.author, self.location)
-    }
-}
-
-pub struct Tweet {
-    pub username: String,
-    pub content: String,
-    pub reply: bool,
-    pub retweet: bool,
-}
-
-impl Summary for Tweet {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
-    }
-}
-
 fn main() {
-    let tweet = Tweet {
-        username: String::from("horse_ebooks"),
-        content: String::from("of course, as you probably already know, people"),
-        reply: false,
-        retweet: false,
-    };
+    let string1 = String::from("abcd");
+    let strig2 = "xyz";
 
-    println!("1 new tweet: {}", tweet.summarize());
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
 }
