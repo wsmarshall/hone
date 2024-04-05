7370dbffbbcf1f27370337d76dd6d8195e930992
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-04-05 14:05:09 -0700

    first pass at 'longest' fn

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+fn longest(x: &str, y: &str) -> &str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
 fn main() {
     let string1 = String::from("abcd");
-    let strig2 = "xyz";
+    let string2 = "xyz";
 
     let result = longest(string1.as_str(), string2);
     println!("The longest string is {}", result);
