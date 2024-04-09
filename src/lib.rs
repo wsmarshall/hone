7d3b87aaa6603c858fd914a0f593603c784e2ec5
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-04-09 09:30:16 -0700

    adds a second test for contrast (pass/fail)

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
+    fn exploration() {
         let result = 2 + 2;
-        assert_eq!(result, 4);
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn another() {
+        panic!("Make this test fail");
     }
 }
