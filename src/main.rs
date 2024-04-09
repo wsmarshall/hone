ff3c9839fb49d6da4ed3ccc73dec02f3e350cf94
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-04-08 17:10:28 -0700

    makes a first example of tests

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() {
-        x
-    } else {
-        y
-    }
+fn add_two(a: i32) -> i32 {
+    a + 2
 }
 
 fn main() {
-    let string1 = String::from("abcd");
-    let string2 = "xyz";
-
-    let result = longest(string1.as_str(), string2);
-    println!("The longest string is {}", result);
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
 }
