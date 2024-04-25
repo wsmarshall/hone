4fc2236a93b50a5a6a5c5f3dffacb04d0adaa771
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-04-25 10:49:12 -0700

    makes a first pass at Drop example

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,20 @@
-use std::ops::Deref;
-
-struct MyBox<T>(T);
-
-impl<T> MyBox<T> {
-    fn new(x: T) -> MyBox<T> {
-        MyBox(x)
-    }
+struct CustomSmartPointer {
+    data: String,
 }
 
-impl<T> Deref for MyBox<T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
     }
 }
 
 fn main() {
-    let m = MyBox::new(String::from("Rust"));
-    hello(&m);
-}
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
 
-fn hello(name: &str) {
-    println!("Hello, {name}!");
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointers created.");
 }
