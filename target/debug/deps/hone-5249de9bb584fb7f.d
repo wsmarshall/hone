ac430f85dd1c311543333d36fe8bd37ef2fca33d
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-05-13 13:36:15 -0700

    makes minor bin changes

--- target/debug/deps/hone-5249de9bb584fb7f.d
+++ target/debug/deps/hone-5249de9bb584fb7f.d
@@ -1,7 +1,7 @@
-/Users/tom/repos/hone/target/debug/deps/libhone-5249de9bb584fb7f.rmeta: src/main.rs src/b03.rs src/b03_tests.rs
+/Users/tom/repos/hone/target/debug/deps/libhone-5249de9bb584fb7f.rmeta: src/main.rs src/b04.rs src/b04_tests.rs
 
-/Users/tom/repos/hone/target/debug/deps/hone-5249de9bb584fb7f.d: src/main.rs src/b03.rs src/b03_tests.rs
+/Users/tom/repos/hone/target/debug/deps/hone-5249de9bb584fb7f.d: src/main.rs src/b04.rs src/b04_tests.rs
 
 src/main.rs:
-src/b03.rs:
-src/b03_tests.rs:
+src/b04.rs:
+src/b04_tests.rs:
