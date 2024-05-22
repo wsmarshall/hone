f76c49a65bc12de57baa76316c031bcd605e2b89
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-05-22 15:25:17 -0700

    starts refactor of naive Node impl to arena (Vec) approach

--- src/node.rs
+++ src/node.rs
@@ -2,14 +2,27 @@
  * a node for trees
  */
 
-struct Node {
-    val: Option<i32>,
-    left: Option<Node>,
-    right: Option<Node>,
+#[derive(Debug, Default)]
+struct ArenaTree<T>
+where
+    T: PartialEq,
+{
+    arena: Vec<Node<T>>,
+}
+
+#[derive(Debug)]
+struct Node<T>
+where
+    T: PartialEq,
+{
+    index: usize,
+    val: T,
+    parent: Option<usize>,
+    children: Vec<usize>,
 }
 
 impl Node {
-    pub fn new(val: Option<i32>, left: Option<i32>, right: Option<i32>) -> Node {
+    pub fn new(val: Option<i32>, left: Option<Box<Node>>, right: Option<Box<Node>>) -> Node {
         Node {
             val: val,
             left: left,
@@ -18,28 +31,28 @@ impl Node {
     }
 
     fn value(&self) -> i32 {
-        unwrap(self.val)
+        self.val.unwrap()
     }
 
-    fn left(&self) -> Option<Node> {
+    fn left(&self) -> Option<Box<Node>> {
         if self.left == None {
             return None;
         }
         return self.left;
     }
 
-    fn right(&self) -> Option<Node> {
+    fn right(&self) -> Option<Box<Node>> {
         if self.right == None {
             return None;
         }
         return self.right;
     }
 
-    fn in_order_traversal(&self, root: Option<Node>) {
+    fn in_order_traversal(&self, root: Option<Box<Node>>) {
         if !(root == None) {
-            self.in_order_traversal(root.unwrap().left());
+            self.in_order_traversal(root.clone().unwrap().left());
             println!("{}", root.unwrap().value());
-            self.in_order_traversal(root.unwrap().right());
+            self.in_order_traversal(root.clone().unwrap().right());
         }
     }
 }
