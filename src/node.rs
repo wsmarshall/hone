0658ea37c5cf9c7cc3334e41c333162aef97ae61
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-05-23 16:34:26 -0700

    conts impl arena Node

--- src/node.rs
+++ src/node.rs
@@ -10,7 +10,24 @@ where
     arena: Vec<Node<T>>,
 }
 
-impl<T> ArenaTree<T> where T: PartialEq {}
+impl<T> ArenaTree<T>
+where
+    T: PartialEq,
+{
+    fn node(&mut self, val: T) -> usize {
+        //check if it exists
+        for node in &self.arena {
+            if node.val == val {
+                return node.index;
+            }
+        }
+
+        //add it if !exist
+        let index = self.arena.len();
+        self.arena.push(Node::new(index, val));
+        index
+    }
+}
 
 #[derive(Debug)]
 struct Node<T>
@@ -37,29 +54,29 @@ where
         }
     }
 
-    fn value(&self) -> i32 {
-        self.val.unwrap()
-    }
+    // fn value(&self) -> i32 {
+    //     self.val.unwrap()
+    // }
 
-    fn left(&self) -> Option<Box<Node>> {
-        if self.left == None {
-            return None;
-        }
-        return self.left;
-    }
+    // fn left(&self) -> Option<Box<Node>> {
+    //     if self.left == None {
+    //         return None;
+    //     }
+    //     return self.left;
+    // }
 
-    fn right(&self) -> Option<Box<Node>> {
-        if self.right == None {
-            return None;
-        }
-        return self.right;
-    }
+    // fn right(&self) -> Option<Box<Node>> {
+    //     if self.right == None {
+    //         return None;
+    //     }
+    //     return self.right;
+    // }
 
-    fn in_order_traversal(&self, root: Option<Box<Node>>) {
-        if !(root == None) {
-            self.in_order_traversal(root.clone().unwrap().left());
-            println!("{}", root.unwrap().value());
-            self.in_order_traversal(root.clone().unwrap().right());
-        }
-    }
+    // fn in_order_traversal(&self, root: Option<Box<Node>>) {
+    //     if !(root == None) {
+    //         self.in_order_traversal(root.clone().unwrap().left());
+    //         println!("{}", root.unwrap().value());
+    //         self.in_order_traversal(root.clone().unwrap().right());
+    //     }
+    // }
 }
