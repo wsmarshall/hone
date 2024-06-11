c9f1a664a6c8a77215f793eaf8ad4b1b7375e94c
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-06-11 13:14:03 -0700

    verifies that build_tree passes second test case (1 2 4 x x 5 x x 3 6 x x 7 x x)

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
 
     let mut test_tree = ArenaTree { arena: Vec::new() };
 
-    test_tree.build_tree("5 4 3 x x 8 x x 6 x x", 2);
+    test_tree.build_tree("1 2 4 x x 5 x x 3 6 x x 7 x x", 2);
 
     // println!(
     //     "in order traversal of test tree is: {}",
