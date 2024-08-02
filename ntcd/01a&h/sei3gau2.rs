e261c8e5ac3174f09eeb94afa7b195da88bd658f
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-08-02 15:31:27 -0700

    fills in first pass for naive approach

--- ntcd/01a&h/sei3gau2.rs
+++ ntcd/01a&h/sei3gau2.rs
@@ -1,3 +1,15 @@
+use std::collections::HashMap;
+
 impl Solution {
-    pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {}
+    pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
+        let mut map = HashMap::new();
+
+        for s in strs {
+            let mut key: Vec<char> = s.chars().collect();
+            key.sort();
+            map.entry(key).or_insert(vec![]).push(s);
+        }
+
+        map.values().cloned().collect()
+    }
 }
