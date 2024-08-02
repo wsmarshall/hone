57cc4c97fc9283da50cdb1d3a4606b5a0c6a1ae6
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-08-01 17:05:54 -0700

    impls a nice one liner on 217

--- ntcd/01a&h/ji6jat1cat1.rs
+++ ntcd/01a&h/ji6jat1cat1.rs
@@ -2,14 +2,6 @@ use std::collections::HashSet;
 
 impl Solution {
     pub fn contains_duplicate(nums: Vec<i32>) -> bool {
-        let mut set = HashSet::<i32>::new();
-        for i in nums {
-            if set.contains(&i) {
-                return true;
-            } else {
-                set.insert(i);
-            }
-        }
-        false
+        nums.len() != HashSet::<i32>::from_iter(nums).len()
     }
 }
