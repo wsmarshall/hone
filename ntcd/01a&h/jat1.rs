dd747854c14773fa58991fe45b24c95bd3ca3b52
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-08-02 11:48:06 -0700

    updates 1 with slightly more concise

--- ntcd/01a&h/jat1.rs
+++ ntcd/01a&h/jat1.rs
@@ -2,24 +2,17 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        //stores leftover (target minus index value), index resp.
-        let mut map = HashMap::<i32, i32>::new();
+        let mut map = HashMap::with_capacity(nums.len());
 
-        //for the answers
-        let mut ans = Vec::<i32>::new();
-
-        for i in 0..nums.len() {
-            let current = nums[i];
-            if (map.contains_key(&current)) {
-                ans.push(*map.get(&current).unwrap_or(&-1));
-                ans.push(i as i32);
-                return ans;
-            }
-            let dif = target - current;
-            map.insert(dif, i as i32);
+        for (i, n) in nums.iter().enumerate() {
+            let j = target - n;
+            match map.get(&j) {
+                Some(e) => return vec![i as i32, *map.get(&j).unwrap()],
+                None => map.insert(n, i as i32),
+            };
         }
 
-        //empty vector if no answer present
-        return Vec::<i32>::new();
+        //unreachable section of code
+        vec![]
     }
 }
