9d6b8a67c3defd6bb1734550727750b6244ba7e2
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-08-05 13:46:42 -0700

    adds fastest 'simple' code (freq table) to group anagrams

--- ntcd/01a&h/sei3gau2.rs
+++ ntcd/01a&h/sei3gau2.rs
@@ -2,14 +2,18 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
-        let mut map = HashMap::new();
-
-        for s in strs {
-            let mut key: Vec<char> = s.chars().collect();
-            key.sort();
-            map.entry(key).or_insert(vec![]).push(s);
+        let mut result: HashMap<[u8; 26], Vec<String>> = HashMap::new();
+        for word in strs {
+            let mut freq: [u8; 26] = [0; 26];
+            for byte in word.bytes() {
+                freq[(byte - b'a') as usize] += 1;
+            }
+            if let Some(words) = result.get_mut(&freq) {
+                words.push(word);
+            } else {
+                result.insert(freq, vec![word]);
+            }
         }
-
-        map.values().cloned().collect()
+        result.into_values().collect()
     }
 }
