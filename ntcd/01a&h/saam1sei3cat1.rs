449b745810939d772c99e95da3d233d3bfd0c1cf
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-08-05 15:40:20 -0700

    most compact, fast but memory intensive 347

--- ntcd/01a&h/saam1sei3cat1.rs
+++ ntcd/01a&h/saam1sei3cat1.rs
@@ -1,3 +1,11 @@
 impl Solution {
-    pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {}
+    pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        let mut map = HashMap::new();
+        nums.iter().for_each(|&i| *map.entry(i).or_insert(0) += 1);
+        let mut sorted = vec![Vec::new(); nums.len()];
+        for (&k, &v) in map.iter() {
+            sorted[nums.len() - v].push(k);
+        }
+        sorted.into_iter().flatten().take(k as usize).collect()
+    }
 }
