8a6942f9a581f07abe2b87455e0f9e1eacf6c26a
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-08-05 17:10:23 -0700

    fills in a nice simple, Rust-y 238 approach

--- ntcd/01a&h/ji6saam1baat3.rs
+++ ntcd/01a&h/ji6saam1baat3.rs
@@ -1,3 +1,26 @@
 impl Solution {
-    pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {}
+    pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
+        /* we will iterate through the array two times:
+        once in the forward direction, giving the multiplication of all the "pre i" entries
+        and similarly in the backward, for all the "post i" entries
+
+        then multiply both those numbers in a carefully placed output array
+        and return
+        */
+        let mut ans = Vec::new();
+        let mut acc = 1;
+        nums.iter().for_each(|x| {
+            ans.push(acc);
+            acc *= x;
+        });
+        acc = 1;
+        //has to be iter -> enumerate -> rev
+        //because enumerate goes left to right natively
+        nums.iter().enumerate().rev().for_each(|(i, x)| {
+            ans[i] *= acc;
+            acc *= x;
+        });
+
+        ans
+    }
 }
