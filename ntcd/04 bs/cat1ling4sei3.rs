7809e7888214e7607d81f94b057981c87572043d
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-10-05 13:21:43 -0700

    tries second pass ntcd bs (passes all tests)

--- ntcd/04 bs/cat1ling4sei3.rs
+++ ntcd/04 bs/cat1ling4sei3.rs
@@ -1,20 +1,19 @@
 impl Solution {
     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-        let mut i = nums.len() / 2;
-        let mut left = 0;
-        let mut right = nums.len();
+        let mut left: usize = 0;
+        let mut right: usize = nums.len();
 
-        while (left <= right) {
-            if nums[i] == target {
-                return i as i32;
-            } else if nums[i] < target {
-                left = i + 1;
-                i = (left + right) / 2;
+        while (left < right) {
+            let mid = left + (right - left) / 2;
+            if nums[mid] == target {
+                return mid as i32;
+            }
+            if nums[mid] < target {
+                left = mid + 1;
             }
             //nums[i] > target
             else {
-                right = i - 1;
-                i = (left + right) / 2;
+                right = mid;
             }
         }
 
