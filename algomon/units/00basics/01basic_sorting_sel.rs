2675218f95f192527ccba83abd5326ad7945a2c7
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-10-08 15:58:33 -0700

    updates selection to use 1 variable fewer

--- algomon/units/00basics/01basic_sorting_sel.rs
+++ algomon/units/00basics/01basic_sorting_sel.rs
@@ -3,20 +3,19 @@
 fn sort_list(unsorted_list: Vec<i32>) -> Vec<i32> {
     let mut list = unsorted_list.clone();
 
+    let mut mindex;
     for i in 0..list.len() {
-        let mut min: i32 = list[i];
         let mut mindex = i;
 
         for j in i..list.len() {
-            if list[j] < min {
-                min = list[j];
+            if list[j] < list[mindex] {
                 mindex = j;
             }
         }
 
-        if min < list[i] {
+        if mindex != i {
             let temp = list[i];
-            list[i] = min;
+            list[i] = list[mindex];
             list[mindex] = temp;
         }
     }
