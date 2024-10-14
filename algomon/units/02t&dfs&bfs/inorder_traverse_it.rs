0affd16b48532023689b975475f7083800b39944
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-10-14 13:34:06 -0700

    starts looking through iterative inorder

--- algomon/units/02t&dfs&bfs/inorder_traverse_it.rs
+++ algomon/units/02t&dfs&bfs/inorder_traverse_it.rs
@@ -1,20 +1,25 @@
 use std::cell::RefCell;
 use std::rc::Rc;
+
 type OptNode = Option<Rc<RefCell<TreeNode>>>;
+
 impl Solution {
     pub fn inorder_traversal(root: OptNode) -> Vec<i32> {
         let mut v = Vec::new();
         let mut stack = vec![(root, false)];
         while let Some((node, push_now)) = stack.pop() {
             if push_now {
+                //'middle' node add
                 v.push(node.unwrap().borrow().val);
             } else if let Some(n) = node {
                 let b = n.borrow();
-                stack.push((b.right.clone(), false));
+                //recall stack: FILO
+                stack.push((b.right.clone(), false)); //what is b.right, and how accessed?
                 stack.push((Some(n.clone()), true));
                 stack.push((b.left.clone(), false));
             }
         }
+
         v
     }
 }
