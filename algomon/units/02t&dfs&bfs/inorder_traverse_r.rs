2ae4649bba3b35117f7a01479e264fe33f65989c
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-10-14 13:35:38 -0700

    adds in (provided) btree node defn (NEEDED)

--- algomon/units/02t&dfs&bfs/inorder_traverse_r.rs
+++ algomon/units/02t&dfs&bfs/inorder_traverse_r.rs
@@ -1,3 +1,23 @@
+// Definition for a binary tree node.
+// PROVIDED
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+
 use std::cell::RefCell;
 use std::rc::Rc;
 
