9395477674326d180e6da4bed7a319bb5a2a5911
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-10-15 16:23:37 -0700

    adds first take on preorder rec

--- algomon/units/02t&dfs&bfs/preorder_traverse_r.rs
+++ algomon/units/02t&dfs&bfs/preorder_traverse_r.rs
@@ -19,5 +19,18 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 impl Solution {
-    pub fn preorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {}
+    pub fn preorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        let mut v = Vec::new();
+        Self::preorder_helper(&root, &mut v);
+        v
+    }
+
+    fn preorder_helper(node: &OptioNode, v: &mut Vec<i32>) {
+        if let Some(n) = node {
+            let b = n.borrow();
+            v.push(b.val);
+            Self::preorder_helper(&b.left, v);
+            Self::preorder_helper(&b.right, v);
+        }
+    }
 }
