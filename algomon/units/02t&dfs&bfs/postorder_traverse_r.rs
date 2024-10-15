ac3d504e2d06259bd8dc99463144ba21196ae277
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-10-15 16:27:47 -0700

    adds first take for postorder rec

--- algomon/units/02t&dfs&bfs/postorder_traverse_r.rs
+++ algomon/units/02t&dfs&bfs/postorder_traverse_r.rs
@@ -18,6 +18,22 @@
 // }
 use std::cell::RefCell;
 use std::rc::Rc;
+
+type OptioNode = Option<Rc<RefCell<TreeNode>>>;
+
 impl Solution {
-    pub fn postorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {}
+    pub fn postorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        let mut v = Vec::new();
+        Self::postorder_helper(&root, &mut v);
+        v
+    }
+
+    fn postorder_helper(node: &OptioNode, v: &mut Vec<i32>) {
+        if let Some(n) = node {
+            let b = n.borrow();
+            Self::postorder_helper(&b.left, v);
+            Self::postorder_helper(&b.right, v);
+            v.push(b.val);
+        }
+    }
 }
