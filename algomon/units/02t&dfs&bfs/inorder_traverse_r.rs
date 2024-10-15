fc0c9b7c077e0ac57272a5011cd9cdcb9e679045
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-10-15 15:11:56 -0700

    updates with current version of inorder (recursive)

--- algomon/units/02t&dfs&bfs/inorder_traverse_r.rs
+++ algomon/units/02t&dfs&bfs/inorder_traverse_r.rs
@@ -21,24 +21,22 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 
-//TreeNode is defined here?
-type OptNode = Option<Rc<RefCell<TreeNode>>>;
+type OptioNode = Option<Rc<RefCell<TreeNode>>>;
 
 impl Solution {
-    pub fn inorder_traversal(root: OptNode) -> Vec<i32> {
+    pub fn inorder_traversal(root: OptioNode) -> Vec<i32> {
         let mut v = Vec::new();
-        Self::inorder(&root, &mut v);
+        Self::inorder_helper(&root, &mut v);
         v
     }
 
-    //helper function for recursive call
-    fn inorder(node: &OptNode, v: &mut Vec<i32>) {
+    fn inorder_helper(node: &OptioNode, v: &mut Vec<i32>) {
         if let Some(n) = node {
             //n is Rc<Refcell<TreeNode>>, b is Refcell<TreeNode>
             let b = n.borrow();
-            Self::inorder(&b.left, v);
+            Self::inorder_helper(&b.left, v);
             v.push(b.val);
-            Self::inorder(&b.right, v);
+            Self::inorder_helper(&b.right, v);
         }
     }
 }
