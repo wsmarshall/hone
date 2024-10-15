5dac546256498ecd7b3e5d63703753598e86eacc
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-10-15 12:28:35 -0700

    stubs binary tree node def'n for preorder files

--- algomon/units/02t&dfs&bfs/inorder_traverse_it.rs
+++ algomon/units/02t&dfs&bfs/inorder_traverse_it.rs
@@ -34,7 +34,7 @@ impl Solution {
             } else if let Some(n) = node {
                 let b = n.borrow();
                 //recall stack: FILO
-                stack.push((b.right.clone(), false)); //what is b.right, and how accessed?
+                stack.push((b.right.clone(), false));
                 stack.push((Some(n.clone()), true));
                 stack.push((b.left.clone(), false));
             }
