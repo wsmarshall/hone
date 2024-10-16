8253d252969fe8e004bb0eea82f1fbbae7ef4b2f
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-10-16 13:23:54 -0700

    adds first pass recursive DFS (along with notes on a curious bug)

---
+++ algomon/units/02t&dfs&bfs/02 dfs/01 dfs_tree.rs
@@ -0,0 +1,44 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn search_bst(
+        root: Option<Rc<RefCell<TreeNode>>>,
+        val: i32,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        if root.is_none() {
+            return None;
+        }
+
+        let binding = root.clone().unwrap();
+        let b = binding.borrow();
+
+        if b.val == val {
+            return root;
+        }
+
+        let left = Self::search_bst(b.left.clone(), val);
+        if left.is_some() {
+            return left;
+        }
+
+        return Self::search_bst(b.right.clone(), val);
+    }
+}
