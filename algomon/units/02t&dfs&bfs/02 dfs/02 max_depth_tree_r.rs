acf6ed75831ca20ed043a941eef2ed8f8a4bfb45
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-10-17 11:49:00 -0700

    adds max_depth_tree_r

---
+++ algomon/units/02t&dfs&bfs/02 dfs/02 max_depth_tree_r.rs
@@ -0,0 +1,33 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+//LC 104 or AM eponymous
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        if root.is_none() {
+            return 0;
+        }
+
+        let binding = root.clone().unwrap();
+        let b = binding.borrow();
+
+        (Self::max_depth(b.left.clone()).max(Self::max_depth(b.right.clone()))) + 1
+    }
+}
