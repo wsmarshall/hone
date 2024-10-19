2175ebb55eedaaa322810360be644a828e94b5e3
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-10-19 14:27:43 -0700

    starts balanced binary tree code

---
+++ algomon/units/02t&dfs&bfs/02 dfs/04 balanced_btree_r.rs
@@ -0,0 +1,16 @@
+fn is_balanced(tree: Tree<i32>) -> bool {
+    if tree.is_none(){
+        return true;
+    }
+    else if let Some(n) = tr
+    let left_height = Self::is_balanced_helper(
+    false
+}
+
+fn tree_height(tree: Tree<i32>) -> i32 {
+    if tree.is_none() {
+        return 0;
+    }
+    
+    
+}
