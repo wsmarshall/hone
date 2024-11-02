0959ee7f92a4d74f4caeeeec881737630b2b7244
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-11-02 15:44:29 -0700

    adds take 2 subtree detection (r) (not working yet)

--- algomon/units/02t&dfs&bfs/02 dfs/05 subtree_detect_r.rs
+++ algomon/units/02t&dfs&bfs/02 dfs/05 subtree_detect_r.rs
@@ -1,16 +1,26 @@
 fn subtree_of_another_tree(root: Tree<i32>, sub_root: Tree<i32>) -> bool {
-    //TODO
-}
-
-fn is_same_tree(tree1: Tree<i32>, tree2: Tree<i32>) -> bool {
+    if let Some(node) = root {
+        if let Some(other_node) = sub_root {
+            return is_same_tree(Some(node.clone()), Some(other_node.clone()))
+                || subtree_of_another_tree(node.left, Some(other_node.clone()))
+                || subtree_of_another_tree(node.right, Some(other_node.clone()));
+        }
+    }
     if root.is_none() && sub_root.is_none() {
         return true;
     }
+    false
+}
 
-    if root.is_none() || sub_root.is_none() {
+fn is_same_tree(tree1: Tree<i32>, tree2: Tree<i32>) -> bool {
+    if tree1.is_none() && tree2.is_none() {
+        return true;
+    }
+
+    if tree1.is_none() || tree2.is_none() {
         return false;
-    } else if let Some(node1) = root {
-        if let Some(node2) = sub_root {
+    } else if let Some(node1) = tree1 {
+        if let Some(node2) = tree2 {
             if node1.val != node2.val {
                 return false;
             } else {
