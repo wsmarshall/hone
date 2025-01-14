09d3d7f403105d365b2ac5f2544f41c023c6d8a7
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-01-14 01:37:56 -0800

    quick tweak to match cases in subtree detect r v2

--- algomon/units/02t&dfs&bfs/02 dfs/05 subtree_detect_r_v2.rs
+++ algomon/units/02t&dfs&bfs/02 dfs/05 subtree_detect_r_v2.rs
@@ -5,8 +5,6 @@ fn subtree_of_another_tree(root: Tree<i32>, sub_root: Tree<i32>) -> bool {
 fn subtree_of_another_tree_r(root: &Tree<i32>, subroot: &Tree<i32>) -> bool {
     match (root, subroot) {
         (None, None) => true,
-        (None, _) => false,
-        (_, None) => false,
         (Some(node1), Some(node2)) => {
             let left = subtree_of_another_tree_r(&node1.left, subroot);
             let right = subtree_of_another_tree_r(&node1.right, subroot);
@@ -20,8 +18,6 @@ fn subtree_of_another_tree_r(root: &Tree<i32>, subroot: &Tree<i32>) -> bool {
 fn is_same(tree1: &Tree<i32>, tree2: &Tree<i32>) -> bool {
     match (tree1, tree2) {
         (None, None) => true,
-        (None, _) => false,
-        (_, None) => false,
         (Some(node1), Some(node2)) => {
             return node1.val == node2.val
                 && is_same(&node1.left, &node2.left)
