51971bb4fe1024397040bf8b5df3f5b9a5f23dcc
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-01-23 23:06:13 -0800

    adds second pass to btree recon r (passes tests)

--- algomon/units/02t&dfs&bfs/02 dfs/07 recon_btree_r.rs
+++ algomon/units/02t&dfs&bfs/02 dfs/07 recon_btree_r.rs
@@ -3,19 +3,26 @@ fn build_btree_r(
     preordex: usize,
     inordex_start: usize,
     size: usize,
-    val_to_index: HashMap,
+    val_to_index: &HashMap<i32, usize>,
 ) -> Tree<i32> {
     if size <= 0 {
         return None;
     }
     let root_val = preorder[preordex];
 
-    let inorder_root_index = val_to_index[root_val];
+    let inorder_root_index = val_to_index[&root_val];
 
     let l_subtree_size = inorder_root_index - inordex_start;
 
-    let left = build_btree_r(preordex + 1, inordex_start, l_subtree_size, &val_to_index);
+    let left = build_btree_r(
+        preorder,
+        preordex + 1,
+        inordex_start,
+        l_subtree_size,
+        &val_to_index,
+    );
     let right = build_btree_r(
+        preorder,
         preordex + 1 + l_subtree_size,
         inorder_root_index + 1,
         size - 1 - l_subtree_size,
@@ -23,9 +30,9 @@ fn build_btree_r(
     );
 
     Some(Box::new(Node {
-        root_val,
-        left,
-        right,
+        val: root_val,
+        left: left,
+        right: right,
     }))
 }
 
@@ -36,5 +43,5 @@ fn construct_binary_tree(preorder: Vec<i32>, inorder: Vec<i32>) -> Tree<i32> {
         .map(|(i, x)| (x, i))
         .collect::<HashMap<_, _>>();
 
-    build_btree_r(&preorder, 0, 0, preorder.len(), inordex)
+    build_btree_r(&preorder, 0, 0, preorder.len(), &inordex)
 }
