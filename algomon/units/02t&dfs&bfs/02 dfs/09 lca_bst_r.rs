67eb93e943a3863d81a63d1db1e6465abdd7066f
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-01-24 22:51:04 -0800

    adds r lca_bst (SIMPLEST CODE)

--- algomon/units/02t&dfs&bfs/02 dfs/09 lca_bst_r.rs
+++ algomon/units/02t&dfs&bfs/02 dfs/09 lca_bst_r.rs
@@ -1,1 +1,14 @@
 //recursive approach, faster than first pass self gen
+
+fn lca_on_bst(bst: Tree<i32>, p: i32, q: i32) -> i32 {
+    if let Some(node) = bst {
+        if p > node.val && q > node.val {
+            return lca_on_bst(node.right, p, q);
+        }
+        if p < node.val && q < node.val {
+            return lca_on_bst(node.left, p, q);
+        }
+        return node.val;
+    }
+    0
+}
