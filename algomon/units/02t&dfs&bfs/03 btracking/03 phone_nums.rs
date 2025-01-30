eb6f1872101df1a8c4fa93b2d837aea7fdcb5a83
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-01-29 22:56:41 -0800

    stubs 03 phone nums

---
+++ algomon/units/02t&dfs&bfs/03 btracking/03 phone_nums.rs
@@ -0,0 +1,21 @@
+fn dfs() {
+    //is leaf
+
+    //get edges
+}
+
+fn letter_combinations_of_phone_number(digits: String) -> Vec<String> {
+    let map = (
+        (),
+        (),
+        ("a", "b", "c"),
+        ("d", "e", "f"),
+        ("g", "h", "i"),
+        ("j", "k", "l"),
+        ("m", "n", "o"),
+        ("p", "q", "r", "s"),
+        ("t", "u", "v"),
+        ("w", "x", "y", "z"),
+    );
+    let number: Vec<&str> = digits.split("").collect();
+}
