93b56c4dedd3773917d8aa5deff6679c229a9667
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-02-03 23:18:39 -0800

    reworks 02 bck_template to use start_index

--- algomon/units/02t&dfs&bfs/03 btracking/02 bck_template.rs
+++ algomon/units/02t&dfs&bfs/03 btracking/02 bck_template.rs
@@ -1,12 +1,18 @@
-fn bck(n: i32, tokens: &Vec<&str>, res: &mut Vec<String>, path: &mut Vec<String>) {
-    if path.len() >= n.try_into().unwrap() {
+fn bck(
+    n: i32,
+    start_index: usize,
+    tokens: &Vec<&str>,
+    res: &mut Vec<String>,
+    path: &mut Vec<String>,
+) {
+    if start_index >= n.try_into().unwrap() {
         res.push(path.join(""));
         return;
     }
 
     for i in tokens {
         path.push(i.clone().to_string());
-        bck(n, tokens, res, path);
+        bck(n, start_index + 1, tokens, res, path);
         path.pop();
     }
 }
@@ -16,7 +22,7 @@ fn letter_combination(n: i32) -> Vec<String> {
     let mut res = Vec::<String>::new();
     let mut path = Vec::<String>::new();
 
-    bck(n, &tokens, &mut res, &mut path);
+    bck(n, 0, &tokens, &mut res, &mut path);
 
     res
 }
