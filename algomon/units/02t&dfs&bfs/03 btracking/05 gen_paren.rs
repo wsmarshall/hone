61c43fade4b363bdd6b518d7c68c62f45bb70847
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-03-05 12:43:54 -0800

    starts fleshing out 05 gen_paren

--- algomon/units/02t&dfs&bfs/03 btracking/05 gen_paren.rs
+++ algomon/units/02t&dfs&bfs/03 btracking/05 gen_paren.rs
@@ -1,3 +1,12 @@
+
+fn dfs (candidates: , results: ,n: usize, start_index: usize, open_count: usize, close_count: usize) {
+
+    if start_index == 2*n {
+        results.push(candidates)
+    }
+}
+
+
 fn generate_parentheses(n: i32) -> Vec<String> {
     // WRITE YOUR BRILLIANT CODE HERE
     Vec::new()
