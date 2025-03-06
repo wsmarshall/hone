dcc972ed94f676f77ecf5dd2c94043e90b68eced
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-03-06 15:44:56 -0800

    creates a version of 06 that runs, but bugfixes incoming (perm, not comb)

--- algomon/units/02t&dfs&bfs/03 btracking/06 gen_perms.rs
+++ algomon/units/02t&dfs&bfs/03 btracking/06 gen_perms.rs
@@ -1,6 +1,23 @@
-fn dfs() {}
+fn dfs(chars: &Vec<char>, candidates: &mut String, answers: &mut Vec<String>, start_index: usize) {
+    if candidates.len() == chars.len() {
+        answers.push(candidates.clone());
+        return;
+    }
+
+    for c in chars {
+        candidates.push(*c);
+        dfs(chars, candidates, answers, start_index + 1);
+        candidates.pop();
+    }
+}
 
 fn permutations(letters: String) -> Vec<String> {
-    // WRITE YOUR BRILLIANT CODE HERE
-    Vec::new()
+    let mut candidates = String::new();
+    let mut answers = Vec::<String>::new();
+
+    let chars: Vec<char> = letters.chars().collect();
+
+    dfs(&chars, &mut candidates, &mut answers, 0);
+
+    answers
 }
