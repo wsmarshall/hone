7638b7a10efb12454b1bff08b953ccb06500dfaf
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-03-08 13:07:59 -0800

    fixes fn sig, passes some tests (bug finding next)

--- algomon/units/02t&dfs&bfs/03 btracking/07 word_break.rs
+++ algomon/units/02t&dfs&bfs/03 btracking/07 word_break.rs
@@ -1,16 +1,17 @@
 fn dfs(s: &str, words: &Vec<String>, start_index: usize) -> bool {
-    
     if start_index == s.len() {
         return true;
     }
-    
-    for i in words:
+
+    for i in words {
         if !i.eq(&s[start_index..(start_index + i.len())]) {
             continue;
         }
-        
-        return dfs(s, words, start_index + i.len())
+
+        return dfs(s, words, start_index + i.len());
     }
+
+    false
 }
 
 fn word_break(s: String, words: Vec<String>) -> bool {
