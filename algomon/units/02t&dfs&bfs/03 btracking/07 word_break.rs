a448d2c4cf048f70fcf16a42f72eaa2aa3e4e9b6
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-03-08 13:07:26 -0800

    adds first pass at 07 word_break

--- algomon/units/02t&dfs&bfs/03 btracking/07 word_break.rs
+++ algomon/units/02t&dfs&bfs/03 btracking/07 word_break.rs
@@ -1,5 +1,18 @@
-fn dfs(s: &str, words: &Vec<String>, start_index: usize) -> bool {}
+fn dfs(s: &str, words: &Vec<String>, start_index: usize) -> bool {
+    
+    if start_index == s.len() {
+        return true;
+    }
+    
+    for i in words:
+        if !i.eq(&s[start_index..(start_index + i.len())]) {
+            continue;
+        }
+        
+        return dfs(s, words, start_index + i.len())
+    }
+}
 
 fn word_break(s: String, words: Vec<String>) -> bool {
-    dfs(&s, &words, 0)
+    dfs(&s, &words, 0) || false
 }
