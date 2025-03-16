f55ab1b296f61490ec98050866b26efcfe21a905
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-03-16 12:51:46 -0700

    starts coding on rm_dupes

--- algomon/units/02tp/01_rm_dupes.rs
+++ algomon/units/02tp/01_rm_dupes.rs
@@ -3,6 +3,18 @@ use std::io;
 use std::str::FromStr;
 
 fn remove_duplicates(arr: &mut Vec<i32>) -> usize {
-    // WRITE YOUR BRILLIANT CODE HERE
-    0
+    let mut left: usize = 0;
+    let mut right: usize = 0;
+
+    let mut prev: i32 = arr[right];
+
+    let len = arr.len();
+
+    while right < len {
+        if arr[left] == arr[right] {
+            right += 1;
+        }
+    }
+
+    left
 }
