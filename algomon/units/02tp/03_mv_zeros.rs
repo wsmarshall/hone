73e9fbbcc08629bc4bb2477646bc81a2e2624653
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-03-19 09:08:06 -0700

    starts coding for 03_mv_zeros

--- algomon/units/02tp/03_mv_zeros.rs
+++ algomon/units/02tp/03_mv_zeros.rs
@@ -1,3 +1,13 @@
 impl Solution {
-    pub fn move_zeroes(nums: &mut Vec<i32>) {}
+    pub fn move_zeroes(nums: &mut Vec<i32>) {
+        let mut left = 0;
+        let mut right = 1;
+
+        while right < nums.len() {
+            if nums[left] == 0 {
+                let temp = nums[left];
+                nums[left] = nums[right];
+            }
+        }
+    }
 }
