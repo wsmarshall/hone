f723e69ccf31815b643d5cadbb611815d3cb771f
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-03-20 10:47:58 -0700

    'rustifies' the mv_zeros soln

--- algomon/units/02tp/03_mv_zeros.rs
+++ algomon/units/02tp/03_mv_zeros.rs
@@ -1,16 +1,12 @@
 impl Solution {
     pub fn move_zeroes(nums: &mut Vec<i32>) {
         let mut left = 0;
-        let mut right = 0;
 
-        while right < nums.len() {
+        for right in 0..nums.len() {
             if nums[right] != 0 {
-                let temp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
+                nums.swap(left, right);
                 left += 1;
             }
-            right += 1;
         }
     }
 }
