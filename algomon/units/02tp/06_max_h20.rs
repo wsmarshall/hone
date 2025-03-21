934f5cc30828ed22424ee61864bd297f367578bb
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-03-20 17:12:36 -0700

    impls max_h20 (passes all tests)

--- algomon/units/02tp/06_max_h20.rs
+++ algomon/units/02tp/06_max_h20.rs
@@ -3,6 +3,21 @@ use std::io;
 use std::str::FromStr;
 
 fn container_with_most_water(arr: Vec<i32>) -> i32 {
-    // WRITE YOUR BRILLIANT CODE HERE
-    0
+    let mut left = 0;
+    let mut right = arr.len() - 1;
+    let mut max = 0;
+    while left < right {
+        let min = std::cmp::min(arr[left], arr[right]);
+        let curr = min * min;
+        if curr > max {
+            max = curr;
+        }
+        if left < right {
+            left += 1;
+        }
+        if right <= left {
+            right -= 1;
+        }
+    }
+    max
 }
