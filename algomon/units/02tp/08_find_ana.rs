46b320928c0aa25d0f750270366c650cdcd774f4
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-03-20 17:57:11 -0700

    makes an (unsuccessful) attempt at HashSet approach for find_ana (use count next)

--- algomon/units/02tp/08_find_ana.rs
+++ algomon/units/02tp/08_find_ana.rs
@@ -5,11 +5,30 @@ use std::io;
 
 fn find_all_anagrams(original: String, check: String) -> Vec<i32> {
     let mut indices = vec![];
-    let mut check = HashSet::new();
-    let chars: Vec<char> = check.chars().collect();
+    let mut reference = HashSet::new();
+    let og_chars: Vec<char> = original.chars().collect();
+    let check_chars: Vec<char> = check.chars().collect();
 
-    for i in chars {
-        check.insert(i);
+    for i in og_chars {
+        reference.insert(i);
+    }
+
+    let mut left = 0;
+    let mut right = original.len();
+    let mut current = HashSet::new();
+
+    for i in left..right {
+        current.insert(&check_chars[i]);
+    }
+
+    while right < check.len() {
+        if current.eq(&reference) {
+            indices.push(left.try_into().unwrap());
+        }
+        current.remove(check_chars[left]);
+        left += 1;
+        current.insert(check_chars[right]);
+        right += 1;
     }
 
     indices
