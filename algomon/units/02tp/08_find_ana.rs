32be53495d6f198d3b2efc4a1d343730d35da3e2
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-03-20 17:36:29 -0700

    stubs find_ana in TP

---
+++ algomon/units/02tp/08_find_ana.rs
@@ -0,0 +1,8 @@
+use std::error;
+use std::fmt::Display;
+use std::io;
+
+fn find_all_anagrams(original: String, check: String) -> Vec<i32> {
+    // WRITE YOUR BRILLIANT CODE HERE
+    Vec::new()
+}
