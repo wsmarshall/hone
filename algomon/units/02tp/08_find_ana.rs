08d4534c54dd377a4bb954410d5107e1e8e35674
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-03-20 17:44:33 -0700

    starts a first pass on 08_find_ana

--- algomon/units/02tp/08_find_ana.rs
+++ algomon/units/02tp/08_find_ana.rs
@@ -1,8 +1,16 @@
+use std::collections::HashSet;
 use std::error;
 use std::fmt::Display;
 use std::io;
 
 fn find_all_anagrams(original: String, check: String) -> Vec<i32> {
-    // WRITE YOUR BRILLIANT CODE HERE
-    Vec::new()
+    let mut indices = vec![];
+    let mut check = HashSet::new();
+    let chars: Vec<char> = check.chars().collect();
+
+    for i in chars {
+        check.insert(i);
+    }
+
+    indices
 }
