257071e9d2a8a2c1b2660bfa6a18704d8c0356a4
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-03-31 15:41:08 -0700

    updates find_ana (runs, but does not pass)

--- algomon/units/02tp/08_find_ana.rs
+++ algomon/units/02tp/08_find_ana.rs
@@ -5,30 +5,38 @@ use std::io;
 
 fn find_all_anagrams(original: String, check: String) -> Vec<i32> {
     let mut indices = vec![];
-    let mut reference = HashSet::new();
+    let mut reference = HashMap::new();
+
     let og_chars: Vec<char> = original.chars().collect();
     let check_chars: Vec<char> = check.chars().collect();
 
     for i in og_chars {
-        reference.insert(i);
+        *reference.entry(i).or_insert(0) += 1;
     }
 
     let mut left = 0;
-    let mut right = original.len();
-    let mut current = HashSet::new();
+    let mut right = check.len() - 1;
+    let mut current = HashMap::new();
 
-    for i in left..right {
-        current.insert(&check_chars[i]);
+    for i in left..check.len() {
+        *current.entry(check_chars[i]).or_insert(0) += 1;
     }
 
-    while right < check.len() {
+    while right < check.len() - 1 {
         if current.eq(&reference) {
             indices.push(left.try_into().unwrap());
         }
-        current.remove(check_chars[left]);
+
+        if let Some(v) = current.get(&check_chars[left]) {
+            if *v == 1 {
+                current.remove(&check_chars[left]);
+            } else {
+                *current.entry(check_chars[left]).or_insert(1) -= 1;
+            }
+        }
         left += 1;
-        current.insert(check_chars[right]);
         right += 1;
+        *current.entry(check_chars[right]).or_insert(0) += 1;
     }
 
     indices
