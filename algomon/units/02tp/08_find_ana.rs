26b3ba6b87205636abfe593b603d0102099604a4
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-04-01 14:42:16 -0700

    adds more to closure/array based find_ana, formats

--- algomon/units/02tp/08_find_ana.rs
+++ algomon/units/02tp/08_find_ana.rs
@@ -3,19 +3,32 @@ use std::error;
 use std::fmt::Display;
 use std::io;
 
+const SIZE: usize = 26;
+
 fn find_all_anagrams(original: String, check: String) -> Vec<i32> {
-    const SIZE: usize = 26;
+    let mut indices = vec![];
 
-    fn find_all_anagrams(original: String, check: String) -> Vec<i32> {
-        let mut indices = vec![];
+    let idx = |c: u8| (c - 'a' as u8) as usize;
 
-        let idx = |c: u8| (c - 'a' as u8) as usize;
-
-        let mut anagram = [0u8; SIZE];
-        for c in check.chars() {
-            anagram[idx(c as u8)] += 1;
-        }
-
-        indices
+    let mut anagram = [0u8; SIZE];
+    for c in check.chars() {
+        anagram[idx(c as u8)] += 1;
     }
+
+    let mut window = [0u8; SIZE];
+    let mut left = 0;
+
+    let og: &[u8] = original.as_bytes();
+    for right in 0..original.len() {
+        window[idx(og[right])] += 1;
+        if right >= check.len() - 1 {
+            left += 1;
+            if anagram == window {
+                indices.push(left as i32);
+            }
+            window[idx(og[left])] -= 1;
+        }
+    }
+
+    indices
 }
