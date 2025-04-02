496aa43dc00d19796b13e554b183f9109b5aa89d
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-04-01 17:41:04 -0700

    fixes find_ana to pass all tests

--- algomon/units/02tp/08_find_ana.rs
+++ algomon/units/02tp/08_find_ana.rs
@@ -19,14 +19,14 @@ fn find_all_anagrams(original: String, check: String) -> Vec<i32> {
     let mut left = 0;
 
     let og: &[u8] = original.as_bytes();
-    for right in 0..original.len() {
+    for right in 0..og.len() {
         window[idx(og[right])] += 1;
         if right >= check.len() - 1 {
-            left += 1;
             if anagram == window {
                 indices.push(left as i32);
             }
             window[idx(og[left])] -= 1;
+            left += 1;
         }
     }
 
