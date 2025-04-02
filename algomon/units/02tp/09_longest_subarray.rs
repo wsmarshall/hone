3ea3ca433a26dc00def9462077840be41a07f5be
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-04-02 12:19:33 -0700

    impls longest subarray (passes all tests)

--- algomon/units/02tp/09_longest_subarray.rs
+++ algomon/units/02tp/09_longest_subarray.rs
@@ -3,6 +3,23 @@ use std::io;
 use std::str::FromStr;
 
 fn subarray_sum_longest(nums: Vec<i32>, target: i32) -> i32 {
-    // WRITE YOUR BRILLIANT CODE HERE
-    0
+    let length = nums.len();
+    let mut max = 0;
+
+    if length == 0 {
+        return max;
+    }
+
+    let mut left = 0;
+    let mut right = 1;
+
+    while right < length {
+        let window = &nums[left..right];
+        let sum: i32 = window.iter().sum();
+        if sum <= target {
+            max = std::cmp::max(max, (right - left + 1) as i32);
+        }
+        right += 1;
+    }
+    max
 }
