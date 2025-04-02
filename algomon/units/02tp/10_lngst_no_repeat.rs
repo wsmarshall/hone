b6a6a526a5e5d37e86ad505757983fa243371b01
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-04-02 15:04:44 -0700

    impls longest_no_repeat (passes all tests)

--- algomon/units/02tp/10_lngst_no_repeat.rs
+++ algomon/units/02tp/10_lngst_no_repeat.rs
@@ -3,14 +3,27 @@ use std::error;
 use std::io;
 
 fn longest_substring_without_repeating_characters(s: String) -> i32 {
-    let mut collection = HashSet::new();
-    let mut max_len = 0;
+    let mut max_len: i32 = 0;
+
+    let length = s.len();
 
     //"base case" for the empty string
-    if s.len() == 0 {
+    if length == 0 {
         return max_len;
     }
 
+    let mut collection = HashSet::new();
+    let chars: Vec<char> = s.chars().collect();
     let mut left = 0;
-    let mut right = 1;
+
+    for right in 0..length {
+        if !collection.contains(&chars[right]) {
+            collection.insert(chars[right]);
+            max_len = std::cmp::max(max_len, collection.len() as i32);
+        } else {
+            collection.remove(&chars[left]);
+            left += 1;
+        }
+    }
+    max_len
 }
