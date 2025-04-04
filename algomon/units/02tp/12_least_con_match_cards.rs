065cc9d6d28cbd7d997123f0e73b556f7a94400e
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-04-04 13:30:47 -0700

    starts code for 12_least_con_match_cards

--- algomon/units/02tp/12_least_con_match_cards.rs
+++ algomon/units/02tp/12_least_con_match_cards.rs
@@ -1,4 +1,17 @@
+use std::error;
+use std::io;
+use std::str::FromStr;
+use std::collections::HashSet;
+
 fn least_consecutive_cards_to_match(cards: Vec<i32>) -> i32 {
-    // WRITE YOUR BRILLIANT CODE HERE
+    let length = cards.len();
+    if length == 0 {
+        return 0;
+    }
+    
+    let mut hand = HashSet::new();
+    let mut left = 0;
+    for right in 0..length
+    
     0
 }
