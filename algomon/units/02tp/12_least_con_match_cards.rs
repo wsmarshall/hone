18069ec3ea825cdb8e0ce815d4fb65f5a90c520f
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-04-05 14:56:21 -0700

    starts swapping to hashmap approach

--- algomon/units/02tp/12_least_con_match_cards.rs
+++ algomon/units/02tp/12_least_con_match_cards.rs
@@ -9,11 +9,13 @@ fn least_consecutive_cards_to_match(cards: Vec<i32>) -> i32 {
         return 0;
     }
     
-    let mut hand = HashSet::new();
+    let mut hand = HashMap::new();
     let mut left = 0;
     let mut least_cards = (length + 1) as i32
     for right in 0..length {
-        
+        if hand.contains(cards[right]) {
+            least_cards = std::cmp::min(least_cards, (right - *hand.entry(cards[right]) + 1) as i32);
+            
         
     }
     
