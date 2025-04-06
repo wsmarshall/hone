0d90c37fa661fab0b9e210ca017dcc8624b7bb1a
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-04-06 15:38:07 -0700

    starts working on HashMap impl for least_con_match_cards

--- algomon/units/02tp/12_least_con_match_cards.rs
+++ algomon/units/02tp/12_least_con_match_cards.rs
@@ -1,23 +1,25 @@
+use std::collections::HashMap;
 use std::error;
 use std::io;
 use std::str::FromStr;
-use std::collections::HashSet;
 
 fn least_consecutive_cards_to_match(cards: Vec<i32>) -> i32 {
     let length = cards.len();
     if length == 0 {
         return 0;
     }
-    
+
     let mut hand = HashMap::new();
     let mut left = 0;
-    let mut least_cards = (length + 1) as i32
+    let mut least_cards = (length + 1) as i32;
     for right in 0..length {
-        if hand.contains(cards[right]) {
-            least_cards = std::cmp::min(least_cards, (right - *hand.entry(cards[right]) + 1) as i32);
-            
-        
+        while hand.contains_key(&cards[right]) {
+            least_cards = std::cmp::min(least_cards, (right - left + 1) as i32);
+            *hand.entry(cards[left]).or_insert(1) -= 1;
+            left += 1;
+        }
+        *hand.entry(cards[right]).or_insert(0) += 1;
     }
-    
+
     least_cards
 }
