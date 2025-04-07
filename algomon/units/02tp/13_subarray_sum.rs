21904898d7888c181a23a0f748b7b9ba348325a8
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-04-07 14:01:02 -0700

    starts a HashMap approach for 13_subarray_sum

--- algomon/units/02tp/13_subarray_sum.rs
+++ algomon/units/02tp/13_subarray_sum.rs
@@ -1,9 +1,14 @@
+use std::collections::HashMap;
 use std::error;
 use std::fmt::Display;
 use std::io;
 use std::str::FromStr;
 
 fn subarray_sum(arr: Vec<i32>, target: i32) -> Vec<i32> {
-    // WRITE YOUR BRILLIANT CODE HERE
+    //holds prefix sum, index
+    let mut pre_sums = HashMap::new();
+    let mut sum = 0;
+    let mut indices = Vec::<i32>::new();
+
     Vec::new()
 }
