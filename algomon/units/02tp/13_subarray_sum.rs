49bf15dd84129315b10169572fb82c79d28a774f
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-04-07 14:32:06 -0700

    updates with current attempt at 13_subarray_sum

--- algomon/units/02tp/13_subarray_sum.rs
+++ algomon/units/02tp/13_subarray_sum.rs
@@ -6,9 +6,20 @@ use std::str::FromStr;
 
 fn subarray_sum(arr: Vec<i32>, target: i32) -> Vec<i32> {
     //holds prefix sum, index
-    let mut pre_sums = HashMap::new();
-    let mut sum = 0;
+    let mut pre_sums: HashMap<i32, usize> = HashMap::new();
+    let mut pre_sum: i32 = 0;
     let mut indices = Vec::<i32>::new();
 
-    Vec::new()
+    for (i, el) in arr.iter().enumerate() {
+        let check = target - el;
+        pre_sum += el;
+        if let Some(n) = pre_sums.get(&check) {
+            indices.push((*n).try_into().unwrap());
+            indices.push(i.try_into().unwrap());
+            break;
+        }
+        pre_sums.insert(pre_sum.try_into().unwrap(), i);
+    }
+
+    indices
 }
