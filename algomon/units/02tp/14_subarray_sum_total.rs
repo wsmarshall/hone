46d9bf01597f73e5f45ae3dbde36d61095acac39
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-04-08 14:43:37 -0700

    adds first pass at 14 (total) -- seems correct but last test failing

---
+++ algomon/units/02tp/14_subarray_sum_total.rs
@@ -0,0 +1,27 @@
+use std::collections::HashMap;
+use std::error;
+use std::io;
+use std::str::FromStr;
+
+fn subarray_sum_total(arr: Vec<i32>, target: i32) -> i32 {
+    let length: i32 = arr.len().try_into().unwrap();
+    if length == 0 {
+        return 0;
+    }
+
+    let mut pre_sums: HashMap<i32, i32> = HashMap::new();
+    pre_sums.insert(0, 1);
+
+    let mut pre_sum: i32 = 0;
+    let mut count = 0;
+    for i in arr {
+        pre_sum += i;
+        *pre_sums.entry(pre_sum).or_insert(0) += 1;
+        let complement = pre_sum - target;
+
+        if let Some(n) = pre_sums.get(&complement) {
+            count += 1;
+        }
+    }
+    count
+}
