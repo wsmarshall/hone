8e5a5aff7a1159605d6f63080b88f4a6712354f9
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-04-08 14:47:51 -0700

    impls 14 (total) -- passes all tests

--- algomon/units/02tp/14_subarray_sum_total.rs
+++ algomon/units/02tp/14_subarray_sum_total.rs
@@ -16,12 +16,13 @@ fn subarray_sum_total(arr: Vec<i32>, target: i32) -> i32 {
     let mut count = 0;
     for i in arr {
         pre_sum += i;
-        *pre_sums.entry(pre_sum).or_insert(0) += 1;
         let complement = pre_sum - target;
 
         if let Some(n) = pre_sums.get(&complement) {
-            count += 1;
+            count += n;
         }
+
+        *pre_sums.entry(pre_sum).or_insert(0) += 1;
     }
     count
 }
