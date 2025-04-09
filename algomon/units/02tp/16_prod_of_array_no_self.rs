4a4a0728476a72d5fb9181c3b690d58011790346
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-04-08 22:38:12 -0700

    impls L/R prod approach for TP 16 (still not passing tests)

--- algomon/units/02tp/16_prod_of_array_no_self.rs
+++ algomon/units/02tp/16_prod_of_array_no_self.rs
@@ -1,17 +1,18 @@
 fn product_of_array_except_self(nums: Vec<i32>) -> Vec<i32> {
     let mut prod = 1;
+    let mut left_prod = vec![];
+
     for i in &nums {
-        if i != &0 {
-            prod *= i;
-        }
+        left_prod.push(prod);
+        prod *= i;
     }
-    let mut ans = Vec::<i32>::new();
-    for i in nums {
-        if i != 0 {
-            ans.push(prod / i);
-        } else {
-            ans.push(prod);
-        }
+
+    let mut ans = vec![];
+
+    prod = 1;
+    for (i, el) in nums.iter().rev().enumerate() {
+        ans.push(prod * left_prod[i]);
+        prod *= el;
     }
 
     ans
