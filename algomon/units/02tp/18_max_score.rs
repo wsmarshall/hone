eaeb2ead3d8d5fc3233a60680a28b95b62815dc9
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-04-14 13:41:27 -0700

    keeps iterating on max_score (not crashing anymore)

--- algomon/units/02tp/18_max_score.rs
+++ algomon/units/02tp/18_max_score.rs
@@ -7,29 +7,31 @@ fn maximum_score(arr1: Vec<i32>, arr2: Vec<i32>) -> i32 {
     let mut e2 = 0;
     let n1 = arr1.len();
     let n2 = arr2.len();
-    let mut sum1 = 0;
-    let mut sum2 = 0;
-    
-    let mut max_score = 0;
-    
-    while e1 < n1 && e2 < n2 {
-        if arr1[e1] == arr2[e2] {
-            max_score += std::cmp::max(sum1, sum2) %  (10^9 + 7);
+    let mut sum1: i32 = 0;
+    let mut sum2: i32 = 0;
+
+    let mut max_score: i32 = 0;
+
+    while e1 < n1 || e2 < n2 {
+        if e1 < n1 && e2 < n2 && arr1[e1] == arr2[e2] {
+            max_score += std::cmp::max(sum1 + arr1[e1], sum2 + arr2[e2]) % MOD;
+            sum1 = 0;
+            sum2 = 0;
             e1 += 1;
             e2 += 1;
+            continue;
         }
-        else if arr1[e1] <= arr2[e2] {
-            sum1 += arr1[e1] %  (10^9 + 7);
+        if e1 < n1 && e2 == n2 {
+            sum1 += arr1[e1] % MOD;
             e1 += 1;
-        } else {//arr1[e1] > arr2[e2]
-            sum2 += arr2[e2] %  (10^9 + 7);
+        } else {
+            //arr1[e1] > arr2[e2]
+            sum2 += arr2[e2] % MOD;
             e2 += 1;
         }
     }
-    
-    while e1 < n1 {
-        
-    
-    
-    max_score
+
+    max_score += std::cmp::max(sum1, sum2);
+
+    max_score % MOD
 }
