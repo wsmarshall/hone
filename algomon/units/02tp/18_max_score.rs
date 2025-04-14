9470a51a8e7361633c1354c7d2745f62346fc160
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-04-14 10:42:52 -0700

    starts first pass attack at max_score

--- algomon/units/02tp/18_max_score.rs
+++ algomon/units/02tp/18_max_score.rs
@@ -3,6 +3,22 @@ use std::io;
 use std::str::FromStr;
 
 fn maximum_score(arr1: Vec<i32>, arr2: Vec<i32>) -> i32 {
-    // WRITE YOUR BRILLIANT CODE HERE
-    0
+    let mut e1 = 0;
+    let mut e2 = 0;
+    let n1 = arr1.len();
+    let n2 = arr2.len();
+
+    let mut max_score = 0;
+
+    while e1 < n1 && e2 < n2 {
+        if arr1[e1] == arr2[e2] {
+        } else if arr1[e1] <= arr2[e2] {
+            e1 += 1;
+        } else {
+            //arr1[e1] > arr2[e2]
+            e2 += 1;
+        }
+    }
+
+    max_score
 }
