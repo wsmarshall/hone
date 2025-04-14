7378f641207cfded640b3313c1f49d4c06231cc4
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-04-14 11:12:55 -0700

    starts trying to fix indexing errors

--- algomon/units/02tp/18_max_score.rs
+++ algomon/units/02tp/18_max_score.rs
@@ -9,21 +9,27 @@ fn maximum_score(arr1: Vec<i32>, arr2: Vec<i32>) -> i32 {
     let n2 = arr2.len();
     let mut sum1 = 0;
     let mut sum2 = 0;
-
+    
     let mut max_score = 0;
-
-    while e1 < n1 || e2 < n2 {
+    
+    while e1 < n1 && e2 < n2 {
         if arr1[e1] == arr2[e2] {
-            max_score += std::cmp::max(sum1, sum2);
-        } else if arr1[e1] <= arr2[e2] || e1 < n1 {
-            sum1 += arr1[e1];
+            max_score += std::cmp::max(sum1, sum2) %  (10^9 + 7);
             e1 += 1;
-        } else {
-            //arr1[e1] > arr2[e2]
-            sum2 += arr2[e2];
+            e2 += 1;
+        }
+        else if arr1[e1] <= arr2[e2] {
+            sum1 += arr1[e1] %  (10^9 + 7);
+            e1 += 1;
+        } else {//arr1[e1] > arr2[e2]
+            sum2 += arr2[e2] %  (10^9 + 7);
             e2 += 1;
         }
     }
-
+    
+    while e1 < n1 {
+        
+    
+    
     max_score
 }
