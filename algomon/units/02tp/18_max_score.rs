a28a69149e8383daf0b16fbe3f4c0f8b763c908b
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-04-14 11:07:22 -0700

    makes second pass at max_score (plays with conditionals)

--- algomon/units/02tp/18_max_score.rs
+++ algomon/units/02tp/18_max_score.rs
@@ -7,15 +7,20 @@ fn maximum_score(arr1: Vec<i32>, arr2: Vec<i32>) -> i32 {
     let mut e2 = 0;
     let n1 = arr1.len();
     let n2 = arr2.len();
+    let mut sum1 = 0;
+    let mut sum2 = 0;
 
     let mut max_score = 0;
 
-    while e1 < n1 && e2 < n2 {
+    while e1 < n1 || e2 < n2 {
         if arr1[e1] == arr2[e2] {
-        } else if arr1[e1] <= arr2[e2] {
+            max_score += std::cmp::max(sum1, sum2);
+        } else if arr1[e1] <= arr2[e2] || e1 < n1 {
+            sum1 += arr1[e1];
             e1 += 1;
         } else {
             //arr1[e1] > arr2[e2]
+            sum2 += arr2[e2];
             e2 += 1;
         }
     }
