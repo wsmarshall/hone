dabd57ee68663e6f2505b350031954554b8a1e12
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-04-16 12:31:48 -0700

    starts making first pass at mindow (TP17)

--- algomon/units/02tp/17_mindow_substring.rs
+++ algomon/units/02tp/17_mindow_substring.rs
@@ -1,7 +1,13 @@
 use std::error;
 use std::io;
 
+const size: usize = 58; //number of characters in ascii 'A' -> 'z'
+                        //NB middle has non-alpha chars
+
 fn get_minimum_window(original: String, check: String) -> String {
-    // WRITE YOUR BRILLIANT CODE HERE
-    String::new()
+    let idx = |c: u8| (c - 'A' as u8) as usize;
+    //TODO use clone_from_slice or copy_from_slice methods later
+    let mut ans: String;
+
+    ans
 }
