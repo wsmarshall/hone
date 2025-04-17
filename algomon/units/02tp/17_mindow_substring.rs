9b8152da80757f85dd52feb82c3e5df0ae4baf9b
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-04-17 11:07:22 -0700

    adds a helper fn for TP 17

--- algomon/units/02tp/17_mindow_substring.rs
+++ algomon/units/02tp/17_mindow_substring.rs
@@ -4,6 +4,19 @@ use std::io;
 const size: usize = 58; //number of characters in ascii 'A' -> 'z'
                         //NB middle has non-alpha chars
 
+
+//checks to see if a slice contains another
+fn contains_helper(original: &[u8], window: &[u8]) -> bool {
+    for i, el in window.iter().enumerate() {
+        if el > 0 {
+            if original[i] != el {
+                return false;
+            }
+        }
+    }
+    true
+}
+
 fn get_minimum_window(original: String, check: String) -> String {
     let idx = |c: u8| (c - 'A' as u8) as usize;
     //TODO use clone_from_slice or copy_from_slice methods later
