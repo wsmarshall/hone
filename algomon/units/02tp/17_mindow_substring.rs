bcbfa8fe333e51af7c70bb5e1f57f75734aed145
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-04-21 10:08:25 -0700

    keeps on TP17, starts as_byte approach

--- algomon/units/02tp/17_mindow_substring.rs
+++ algomon/units/02tp/17_mindow_substring.rs
@@ -2,10 +2,9 @@ use std::error;
 use std::io;
 
 const size: usize = 58; //number of characters in ascii 'A' -> 'z'
-                        //NB middle has non-alpha chars
+//NB middle has non-alpha chars
 
-
-//checks to see if a slice contains another
+//checks to see if a slice contains another wrt character counts
 fn contains_helper(original: &[u8], window: &[u8]) -> bool {
     for i, el in window.iter().enumerate() {
         if el > 0 {
@@ -18,9 +17,26 @@ fn contains_helper(original: &[u8], window: &[u8]) -> bool {
 }
 
 fn get_minimum_window(original: String, check: String) -> String {
-    let idx = |c: u8| (c - 'A' as u8) as usize;
+    let og_bytes = original.as_bytes();
+    let check_bytes = check.as_bytes();
+    
+    let idx = |c: u8| {(c - 'A' as u8) as usize};
     //TODO use clone_from_slice or copy_from_slice methods later
+    let len = original.len();
+    
+    let mut og = [0u8; size];
+    for i in 0..len {
+        
+    }
+    
+    let mut window = [0u8; size];
+    let mut left = 0;
+    
+    for right in 0..len {
+        window[idx(original.chars().nth(right))] += 1;
+        while 
+    }
     let mut ans: String;
-
+    
     ans
 }
