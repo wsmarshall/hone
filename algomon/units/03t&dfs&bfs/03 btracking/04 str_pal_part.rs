51cb7d37f0e72233d79e302feced103306820562
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-04-30 10:19:56 -0700

    take 2 on str part pal

--- algomon/units/03t&dfs&bfs/03 btracking/04 str_pal_part.rs
+++ algomon/units/03t&dfs&bfs/03 btracking/04 str_pal_part.rs
@@ -1,37 +1,31 @@
-fn is_palindrome(s: &str) -> bool {
+fn is_pal(s: &str) -> bool {
     s.chars().eq(s.chars().rev())
 }
 
-fn dfs(
-    s: &String,
-    answers: &mut Vec<Vec<String>>,
-    candidates: &mut Vec<String>,
-    start_index: usize,
-) {
+fn dfs(s: &String, start_index: usize, ans: &mut Vec<Vec<String>>, path: &mut Vec<String>) {
     if start_index == s.len() {
-        answers.push(candidates.clone());
+        ans.push(path.clone());
         return;
     }
 
     for i in start_index..s.len() {
         let candidate = &s[start_index..i + 1];
 
-        if !is_palindrome(candidate) {
+        if !is_pal(candidate) {
             continue;
         }
 
-        candidates.push(candidate.to_string());
-        dfs(s, answers, candidates, i + 1);
-        candidates.pop();
+        path.push(candidate.to_string());
+        dfs(&s, i + 1, ans, path);
+        path.pop();
     }
 }
 
 fn partition(s: String) -> Vec<Vec<String>> {
-    let mut candidates = Vec::<String>::new();
+    let mut ans: Vec<Vec<String>> = vec![];
+    let mut path: Vec<String> = vec![];
 
-    let mut answers = Vec::<Vec<String>>::new();
+    dfs(&s, 0, &mut ans, &mut path);
 
-    dfs(&s, &mut answers, &mut candidates, 0);
-
-    answers
+    ans
 }
