f99e182bf9e8c586e2ce789cc9533034250392cf
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-05-01 12:40:42 -0700

    finalizes btrack 07 (passes all tests)

--- algomon/units/03t&dfs&bfs/03 btracking/07 word_break.rs
+++ algomon/units/03t&dfs&bfs/03 btracking/07 word_break.rs
@@ -1,18 +1,23 @@
-fn dfs(s: &str, words: &Vec<String>, start_index: usize) -> bool {
+fn dfs(s: &str, words: &Vec<String>, start_index: usize, memo: &mut Vec<Option<bool>>) -> bool {
     if start_index == s.len() {
         return true;
+    }
+
+    if let Some(b) = memo[start_index] {
+        return b;
     }
 
     let mut ans = false;
     for i in words {
         if s[start_index..].starts_with(i) {
-            ans = ans || dfs(s, words, start_index + i.len());
+            ans = ans || dfs(s, words, start_index + i.len(), memo);
         }
     }
-
+    memo[start_index] = Some(ans);
     ans
 }
 
 fn word_break(s: String, words: Vec<String>) -> bool {
-    dfs(&s, &words, 0)
+    let mut memo = vec![None; s.len()];
+    dfs(&s, &words, 0, &mut memo)
 }
