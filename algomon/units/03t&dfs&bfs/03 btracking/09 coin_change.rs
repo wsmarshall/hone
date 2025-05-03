a0702a0876686ee4a3a195ea2542be242553ae3b
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-05-03 15:12:00 -0700

    adds memoization for btrack 09 (4/5 pass)

--- algomon/units/03t&dfs&bfs/03 btracking/09 coin_change.rs
+++ algomon/units/03t&dfs&bfs/03 btracking/09 coin_change.rs
@@ -1,25 +1,29 @@
-fn dfs(coins: &Vec<i32>, amount: i32, sum: i32) -> i32 {
+fn dfs(coins: &Vec<i32>, amount: i32, sum: i32, memo: &mut Vec<i32>) -> i32 {
     if sum == amount {
         return 0;
     }
     if sum > amount {
         return std::i32::MAX;
     }
+    if memo[sum as usize] != -1 {
+        return memo[sum as usize];
+    }
 
     let mut ans = std::i32::MAX;
     for i in coins {
-        let mut result = dfs(coins, amount, sum + i);
+        let mut result = dfs(coins, amount, sum + i, memo);
         if result == std::i32::MAX {
             continue;
         }
         ans = std::cmp::min(result + 1, std::i32::MAX);
     }
-
+    memo[sum as usize] = ans;
     ans
 }
 
 fn coin_change(coins: Vec<i32>, amount: i32) -> i32 {
-    let result = dfs(&coins, amount, 0);
+    let mut memo: Vec<i32> = vec![-1; (amount + 1).try_into().unwrap()];
+    let result = dfs(&coins, amount, 0, &mut memo);
     if result == std::i32::MAX {
         return -1;
     }
