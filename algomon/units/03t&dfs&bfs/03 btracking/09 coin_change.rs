2414592e1bb0b08bf0235e109f9f7ff63472faa3
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-05-03 15:03:32 -0700

    adds first pass at coin change (btrack 09) (4/5 tests pass)

--- algomon/units/03t&dfs&bfs/03 btracking/09 coin_change.rs
+++ algomon/units/03t&dfs&bfs/03 btracking/09 coin_change.rs
@@ -1,4 +1,28 @@
+fn dfs(coins: &Vec<i32>, amount: i32, sum: i32) -> i32 {
+    if sum == amount {
+        return 0;
+    }
+    if sum > amount {
+        return std::i32::MAX;
+    }
+
+    let mut ans = std::i32::MAX;
+    for i in coins {
+        let mut result = dfs(coins, amount, sum + i);
+        if result == std::i32::MAX {
+            continue;
+        }
+        ans = std::cmp::min(result + 1, std::i32::MAX);
+    }
+
+    ans
+}
+
 fn coin_change(coins: Vec<i32>, amount: i32) -> i32 {
-    // WRITE YOUR BRILLIANT CODE HERE
-    0
+    let mut result = -1;
+    result = dfs(&coins, amount, 0);
+    if result == std::i32::MAX {
+        return -1;
+    }
+    result
 }
