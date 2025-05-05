933ec95f5e937dcde98f599ce98fd4d7dfdaf12c
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-05-05 15:18:36 -0700

    adds first pass (dfs and dedup) for dfs11 subsets (passes all tests)

--- algomon/units/03t&dfs&bfs/03 btracking/11 subsets.rs
+++ algomon/units/03t&dfs&bfs/03 btracking/11 subsets.rs
@@ -1,4 +1,17 @@
+fn dfs(nums: &Vec<i32>, answers: &mut Vec<Vec<i32>>, current: &mut Vec<i32>, start_index: usize) {
+    answers.push(current.clone());
+    for i in start_index..nums.len() {
+        current.push(nums[i]);
+        dfs(nums, answers, current, i + 1);
+        current.pop();
+    }
+}
+
 fn subsets(nums: Vec<i32>) -> Vec<Vec<i32>> {
-    // WRITE YOUR BRILLIANT CODE HERE
-    Vec::new()
+    let mut answers: Vec<Vec<i32>> = vec![];
+    let mut current: Vec<i32> = vec![];
+
+    dfs(&nums, &mut answers, &mut current, 0);
+
+    answers
 }
