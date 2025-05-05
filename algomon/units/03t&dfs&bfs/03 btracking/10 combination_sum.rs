1558d3a50bef30c249a419cb8fc2c0a989657ef0
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-05-05 13:47:05 -0700

    tries first pass at dfs10 combination_sum

--- algomon/units/03t&dfs&bfs/03 btracking/10 combination_sum.rs
+++ algomon/units/03t&dfs&bfs/03 btracking/10 combination_sum.rs
@@ -1,4 +1,36 @@
-fn combination_sum(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
-    // WRITE YOUR BRILLIANT CODE HERE
-    Vec::new()
+fn dfs(
+    candidates: &Vec<i32>,
+    answers: &mut Vec<Vec<i32>>,
+    path: &mut Vec<i32>,
+    target: i32,
+    mut sum: i32,
+    start_index: usize,
+) {
+    if sum == target {
+        answers.push(path.clone());
+        return;
+    }
+    if sum < target {
+        for i in start_index..candidates.len() {
+            if sum + candidates[i] > sum {
+                break;
+            }
+            sum += candidates[i];
+            path.push(candidates[i]);
+            dfs(candidates, answers, path, target, sum, start_index + 1);
+            path.pop();
+            sum -= candidates[i];
+        }
+    }
+}
+
+fn combination_sum(mut candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
+    let mut answers: Vec<Vec<i32>> = vec![vec![]];
+    let mut path: Vec<i32> = vec![];
+
+    candidates.sort();
+
+    dfs(&candidates, &mut answers, &mut path, target, 0, 0);
+
+    answers
 }
