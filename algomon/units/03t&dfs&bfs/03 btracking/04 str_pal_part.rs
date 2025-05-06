c98ce2559dffe43a68f29a3c5fb4186152d1a47d
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-05-06 12:52:11 -0700

    revises btrack 04 (shorter)

--- algomon/units/03t&dfs&bfs/03 btracking/04 str_pal_part.rs
+++ algomon/units/03t&dfs&bfs/03 btracking/04 str_pal_part.rs
@@ -2,30 +2,26 @@ fn is_pal(s: &str) -> bool {
     s.chars().eq(s.chars().rev())
 }
 
-fn dfs(s: &String, start_index: usize, ans: &mut Vec<Vec<String>>, path: &mut Vec<String>) {
+fn dfs(s: &str, answers: &mut Vec<Vec<String>>, path: &mut Vec<String>, start_index: usize) {
     if start_index == s.len() {
-        ans.push(path.clone());
+        answers.push(path.clone());
         return;
     }
 
     for i in start_index..s.len() {
-        let candidate = &s[start_index..i + 1];
-
-        if !is_pal(candidate) {
-            continue;
+        if is_pal(&s[start_index..i + 1]) {
+            path.push(s[start_index..i + 1].to_string());
+            dfs(s, answers, path, i + 1);
+            path.pop();
         }
-
-        path.push(candidate.to_string());
-        dfs(&s, i + 1, ans, path);
-        path.pop();
     }
 }
 
 fn partition(s: String) -> Vec<Vec<String>> {
-    let mut ans: Vec<Vec<String>> = vec![];
+    let mut answers: Vec<Vec<String>> = vec![];
     let mut path: Vec<String> = vec![];
 
-    dfs(&s, 0, &mut ans, &mut path);
+    dfs(&s, &mut answers, &mut path, 0);
 
-    ans
+    answers
 }
