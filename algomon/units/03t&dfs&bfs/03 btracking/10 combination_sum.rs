aed40162c4d02a9a571b8f48062be68595cca7d4
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-05-10 14:34:30 -0700

    adds alt soln, notes for btrack 10

--- algomon/units/03t&dfs&bfs/03 btracking/10 combination_sum.rs
+++ algomon/units/03t&dfs&bfs/03 btracking/10 combination_sum.rs
@@ -35,3 +35,36 @@ fn combination_sum(mut candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
 
     answers
 }
+
+
+/**
+ * CODE WORKS WITH NO SORTING??
+ * 
+ * fn dfs(candidates: &Vec<i32>, target: i32, sum: i32, answers: &mut Vec<Vec<i32>>, path: &mut Vec<i32>, start_index: usize) {
+    
+    if sum == target {
+        answers.push(path.clone());
+    }
+    
+    if sum > target {
+        return;
+    }
+    
+    for i in start_index..candidates.len() {
+        path.push(candidates[i]);
+        dfs(candidates, target, sum+candidates[i], answers, path, i);
+        path.pop();
+    }
+    
+    
+}
+
+fn combination_sum(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
+    let mut answers: Vec::<Vec::<i32>> = vec![];
+    let mut path: Vec::<i32> = vec![];
+    
+    dfs(&candidates, target, 0, &mut answers, &mut path, 0);
+    
+    answers
+}
+ */
