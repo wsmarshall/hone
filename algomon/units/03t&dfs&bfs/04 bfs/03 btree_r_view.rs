757df74a532fef9aa5079947e39f643dc84344f4
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-05-11 13:09:16 -0700

    finishes bfs03 btree r view

--- algomon/units/03t&dfs&bfs/04 bfs/03 btree_r_view.rs
+++ algomon/units/03t&dfs&bfs/04 bfs/03 btree_r_view.rs
@@ -14,4 +14,30 @@ struct Node<T> {
 
 fn binary_tree_right_side_view(root: Tree<i32>) -> Vec<i32> {
     let mut queue = VecDeque::new();
+    queue.push_back(root);
+    let mut traversal: Vec<Vec<i32>> = vec![];
+    let mut right_side_view: Vec<i32> = vec![];
+
+    while !queue.is_empty() {
+        let mut level = vec![];
+        for i in 0..queue.len() {
+            if let Some(Some(n)) = queue.pop_front() {
+                level.push(n.val);
+
+                if n.left.is_some() {
+                    queue.push_back(n.left);
+                }
+                if n.right.is_some() {
+                    queue.push_back(n.right);
+                }
+            }
+        }
+        traversal.push(level);
+    }
+
+    for mut i in traversal {
+        right_side_view.push(i.pop().expect("bug"));
+    }
+
+    right_side_view
 }
