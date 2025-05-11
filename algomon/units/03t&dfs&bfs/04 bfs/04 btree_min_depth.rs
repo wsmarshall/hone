cdb779b2175332e730bb0a4617a838146badc5f6
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-05-11 13:17:41 -0700

    impls bfs04 (passes all tests)

--- algomon/units/03t&dfs&bfs/04 bfs/04 btree_min_depth.rs
+++ algomon/units/03t&dfs&bfs/04 bfs/04 btree_min_depth.rs
@@ -1,3 +1,4 @@
+use std::collections::VecDeque;
 use std::error;
 use std::io;
 use std::str::FromStr;
@@ -11,6 +12,25 @@ struct Node<T> {
 }
 
 fn binary_tree_min_depth(root: Tree<i32>) -> i32 {
-    // WRITE YOUR BRILLIANT CODE HERE
-    0
+    let mut queue = VecDeque::new();
+    queue.push_back(root);
+    let mut depth = 0;
+    while !queue.is_empty() {
+        for i in 0..queue.len() {
+            if let Some(Some(n)) = queue.pop_front() {
+                if n.left.is_none() && n.right.is_none() {
+                    return depth;
+                }
+
+                if n.left.is_some() {
+                    queue.push_back(n.left);
+                }
+                if n.right.is_some() {
+                    queue.push_back(n.right);
+                }
+            }
+        }
+        depth += 1;
+    }
+    -1
 }
