23c77a26dcd4c407490f6bd4b77865187191cb38
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-05-11 12:44:16 -0700

    makes 'final' v of bfs01

--- algomon/units/03t&dfs&bfs/04 bfs/01 level_order_traversal_r.rs
+++ algomon/units/03t&dfs&bfs/04 bfs/01 level_order_traversal_r.rs
@@ -13,26 +13,27 @@ struct Node<T> {
 }
 
 fn level_order_traversal(root: Tree<i32>) -> Vec<Vec<i32>> {
-    let mut answer: Vec<Vec<i32>> = vec![];
     let mut queue = VecDeque::new();
     queue.push_back(root);
+    let mut traversal: Vec<Vec<i32>> = vec![];
 
     while !queue.is_empty() {
-        let mut level_list = vec![];
-        for i in 0..queue.len() {
-            if let Some(Some(current_node)) = queue.pop_front() {
-                level_list.push(current_node.val);
+        let mut level: Vec<i32> = vec![];
 
-                if current_node.left.is_some() {
-                    queue.push_back(current_node.left);
+        for i in 0..queue.len() {
+            if let Some(Some(n)) = queue.pop_front() {
+                level.push(n.val);
+
+                if n.left.is_some() {
+                    queue.push_back(n.left);
                 }
 
-                if current_node.right.is_some() {
-                    queue.push_back(current_node.right);
+                if n.right.is_some() {
+                    queue.push_back(n.right);
                 }
             }
         }
-        answer.push(level_list);
+        traversal.push(level);
     }
-    answer
+    traversal
 }
