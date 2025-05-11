3749c8c4c124ce03702896395449375e14b45181
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-05-11 12:16:06 -0700

    makes small tweaks to btrack 09

--- algomon/units/03t&dfs&bfs/03 btracking/09 coin_change.rs
+++ algomon/units/03t&dfs&bfs/03 btracking/09 coin_change.rs
@@ -1,30 +1,30 @@
-fn dfs(coins: &Vec<i32>, amount: i32, sum: i32, memo: &mut Vec<i32>) -> i32 {
-    if sum == amount {
+fn dfs(denoms: &Vec<i32>, target: i32, sum: i32, start_index: usize, memo: &mut Vec<i32>) -> i32 {
+    if sum == target {
         return 0;
     }
-    if sum > amount {
-        return std::i32::MAX;
+
+    if sum > target {
+        return std::i32::MAX - 9;
     }
+
     if memo[sum as usize] != -1 {
         return memo[sum as usize];
     }
 
-    let mut ans = std::i32::MAX;
-    for i in coins {
-        let mut result = dfs(coins, amount, sum + i, memo);
-        if result == std::i32::MAX {
-            continue;
-        }
-        ans = std::cmp::min(result + 1, ans);
+    let mut inter_result = std::i32::MAX - 9;
+    for i in start_index..denoms.len() {
+        let mut temp;
+        temp = dfs(denoms, target, sum + denoms[i], i, memo);
+        inter_result = std::cmp::min(inter_result, temp + 1);
     }
-    memo[sum as usize] = ans;
-    ans
+    memo[sum as usize] = inter_result;
+    inter_result
 }
 
 fn coin_change(coins: Vec<i32>, amount: i32) -> i32 {
-    let mut memo: Vec<i32> = vec![-1; (amount + 1).try_into().unwrap()];
-    let result = dfs(&coins, amount, 0, &mut memo);
-    if result == std::i32::MAX {
+    let mut memo = vec![-1; (amount + 1).try_into().unwrap()];
+    let result = dfs(&coins, amount, 0, 0, &mut memo);
+    if result == std::i32::MAX - 9 {
         return -1;
     }
     result
