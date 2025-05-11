47dc6907f2c601156631d4aaec7dbb2b1b293cb4
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-05-11 12:59:26 -0700

    insts a working zigzag level order traversal (bfs02)

--- algomon/units/03t&dfs&bfs/04 bfs/02 zigzag_level_order_traversal.rs
+++ algomon/units/03t&dfs&bfs/04 bfs/02 zigzag_level_order_traversal.rs
@@ -1,3 +1,4 @@
+use std::collections::VecDeque;
 use std::error;
 use std::fmt::Display;
 use std::io;
@@ -12,6 +13,32 @@ struct Node<T> {
 }
 
 fn zig_zag_traversal(root: Tree<i32>) -> Vec<Vec<i32>> {
-    // WRITE YOUR BRILLIANT CODE HERE
-    Vec::new()
+    let mut l2r: bool = true;
+    let mut queue = VecDeque::new();
+    queue.push_back(root);
+    let mut traversal: Vec<Vec<i32>> = vec![];
+
+    while !queue.is_empty() {
+        let mut next_level = vec![];
+        for i in 0..queue.len() {
+            if let Some(Some(n)) = queue.pop_front() {
+                if l2r {
+                    next_level.push(n.val);
+                } else {
+                    next_level.insert(0, n.val);
+                }
+
+                if n.left.is_some() {
+                    queue.push_back(n.left);
+                }
+
+                if n.right.is_some() {
+                    queue.push_back(n.right);
+                }
+            }
+        }
+        traversal.push(next_level);
+        l2r = !l2r;
+    }
+    traversal
 }
