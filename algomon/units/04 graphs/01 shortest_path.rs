d29d4c2d867fedf2cf51ed4f928e6146043a1bd9
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-05-17 14:59:40 -0700

    starts bfs graph shortest path

--- algomon/units/04 graphs/01 shortest_path.rs
+++ algomon/units/04 graphs/01 shortest_path.rs
@@ -5,6 +5,23 @@ use std::io;
 use std::str::FromStr;
 
 fn shortest_path(graph: Vec<Vec<i32>>, a: i32, b: i32) -> i32 {
-    // WRITE YOUR BRILLIANT CODE HERE
-    0
+    let mut queue = VecDeque::new();
+    queue.push_back(graph[a as usize]);
+    let mut visited = HashSet::new();
+
+    let mut distance = 0;
+
+    while !queue.is_empty() {
+        if let Some(current) = queue.pop_front() {
+            for i in 0..current.len() {
+                if visited.insert(current) {
+                    queue.push_back(graph[i]);
+                } else {
+                    continue;
+                }
+            }
+        }
+    }
+
+    distance
 }
