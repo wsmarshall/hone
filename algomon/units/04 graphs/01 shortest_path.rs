c22768c3811b24433f1ce4402b55abcc666de84e
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-05-18 16:08:10 -0700

    finishes(?) bfs graph 01

--- algomon/units/04 graphs/01 shortest_path.rs
+++ algomon/units/04 graphs/01 shortest_path.rs
@@ -8,20 +8,26 @@ fn shortest_path(graph: Vec<Vec<i32>>, a: i32, b: i32) -> i32 {
     let mut queue: VecDeque<i32> = VecDeque::new();
     queue.push_back(a);
     let mut visited: HashSet<i32> = HashSet::new();
+    visited.insert(a);
 
     let mut distance = 0;
+    if a == b {
+        return distance;
+    }
 
     while !queue.is_empty() {
         distance += 1;
-        if let Some(num) = queue.pop_front() {
-            let current_node_neighbors = &graph[num as usize];
-            for i in 0..current_node_neighbors.len() {
-                if current_node_neighbors[i] == b {
-                    return distance;
-                } else if visited.insert(current_node_neighbors[i]) {
-                    queue.push_back(current_node_neighbors[i]);
-                } else {
-                    continue;
+        for i in 0..queue.len() {
+            if let Some(current_node) = queue.pop_front() {
+                let current_node_neighbors = &graph[current_node as usize];
+                for j in 0..current_node_neighbors.len() {
+                    if current_node_neighbors[i] == b {
+                        return distance;
+                    } else if visited.insert(current_node_neighbors[i]) {
+                        queue.push_back(current_node_neighbors[i]);
+                    } else {
+                        continue;
+                    }
                 }
             }
         }
