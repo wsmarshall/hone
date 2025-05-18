7a497127368d30cf3a46cb620322da3dc012b197
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-05-18 15:55:39 -0700

    conts bfs graph 01 (3/4 tests pass, find way to do distance correct)

--- algomon/units/04 graphs/01 shortest_path.rs
+++ algomon/units/04 graphs/01 shortest_path.rs
@@ -5,25 +5,25 @@ use std::io;
 use std::str::FromStr;
 
 fn shortest_path(graph: Vec<Vec<i32>>, a: i32, b: i32) -> i32 {
-    let mut queue = VecDeque::new();
+    let mut queue: VecDeque<i32> = VecDeque::new();
     queue.push_back(a);
-    let mut visited = HashSet::new();
+    let mut visited: HashSet<i32> = HashSet::new();
 
     let mut distance = 0;
 
     while !queue.is_empty() {
+        distance += 1;
         if let Some(num) = queue.pop_front() {
             let current_node_neighbors = &graph[num as usize];
             for i in 0..current_node_neighbors.len() {
-                if visited.insert(current_node_neighbors[i]) {
-                    queue.push_back(current_node_neighbors[i]);
-                } else if current_node_neighbors[i] == b {
+                if current_node_neighbors[i] == b {
                     return distance;
+                } else if visited.insert(current_node_neighbors[i]) {
+                    queue.push_back(current_node_neighbors[i]);
                 } else {
                     continue;
                 }
             }
-            distance += 1;
         }
     }
     distance
