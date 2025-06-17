285e3e15231de98db959242db9c8cae333882680
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-06-17 16:02:44 -0700

    alters graph04 get_neighbors to fit parameters of problem

--- algomon/units/04 graphs/04 k_min_moves.rs
+++ algomon/units/04 graphs/04 k_min_moves.rs
@@ -16,20 +16,13 @@ fn build_Coordinate(r: i32, c: i32, val: i32) -> Coordinate {
 
 fn get_neighbors(coord: Coordinate, grid: &Vec<Vec<i32>>) -> Vec<Coordinate> {
     let mut neighbors: Vec<Coordinate> = vec![];
-    let deltaRow = [-1, 0, 1, 0];
-    let deltaCol = [0, 1, 0, -1];
+    let deltaRow = [1, 2, 2, 1, -1, -2, -2, -1];
+    let deltaCol = [-2, -1, 1, 2, 2, 1, -1, -2];
     for i in 0..deltaRow.len() {
         let neighborRow = coord.r as i32 + deltaRow[i];
         let neighborCol = coord.c as i32 + deltaCol[i];
-        if 0 <= neighborRow
-            && neighborRow < grid.len().try_into().unwrap()
-            && 0 <= neighborCol
-            && neighborCol < grid[0].len().try_into().unwrap()
-        {
-            if grid[neighborRow as usize][neighborCol as usize] == 1 {
-                neighbors.push(build_Coordinate(neighborRow, neighborCol, 1));
-            }
-        }
+
+        neighbors.push(build_Coordinate(neighborRow, neighborCol));
     }
 
     neighbors
