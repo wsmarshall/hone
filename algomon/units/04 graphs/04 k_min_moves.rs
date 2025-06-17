91e85431e581867204b07b87676d02ed4bbcb4b2
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-06-17 16:37:24 -0700

    fixes syntax errors for graph04 (tests DO NOT pass)

--- algomon/units/04 graphs/04 k_min_moves.rs
+++ algomon/units/04 graphs/04 k_min_moves.rs
@@ -2,19 +2,18 @@ use std::collections::HashSet;
 use std::collections::VecDeque;
 use std::error;
 use std::io;
-use std::str::FromStr;
 
 #[derive(Clone, Eq, Hash, PartialEq)]
 struct Coordinate {
-    r: usize,
-    c: usize,
+    r: i32,
+    c: i32,
 }
 
-fn build_Coordinate(r: i32, c: i32, val: i32) -> Coordinate {
+fn build_Coordinate(r: i32, c: i32) -> Coordinate {
     Coordinate { r: r, c: c }
 }
 
-fn get_neighbors(coord: Coordinate, grid: &Vec<Vec<i32>>) -> Vec<Coordinate> {
+fn get_neighbors(coord: &Coordinate) -> Vec<Coordinate> {
     let mut neighbors: Vec<Coordinate> = vec![];
     let deltaRow = [1, 2, 2, 1, -1, -2, -2, -1];
     let deltaCol = [-2, -1, 1, 2, 2, 1, -1, -2];
@@ -28,7 +27,29 @@ fn get_neighbors(coord: Coordinate, grid: &Vec<Vec<i32>>) -> Vec<Coordinate> {
     neighbors
 }
 
-fn bfs(x: i32, y: i32) -> i32 {}
+fn bfs(x: i32, y: i32) -> i32 {
+    let mut visited: HashSet<Coordinate> = HashSet::new();
+    let mut queue: VecDeque<Coordinate> = VecDeque::new();
+    let mut steps: i32 = 0;
+
+    queue.push_back(build_Coordinate(x, y));
+
+    while !queue.is_empty() {
+        if let Some(coord) = queue.pop_front() {
+            if coord.r == x && coord.c == y {
+                break;
+            }
+
+            let neighbors = get_neighbors(&coord);
+            for i in neighbors {
+                queue.push_back(i);
+            }
+            steps += 1;
+        }
+    }
+
+    steps
+}
 
 fn get_knight_shortest_path(x: i32, y: i32) -> i32 {
     bfs(x, y)
