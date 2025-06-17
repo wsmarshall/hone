2bc367d5574038e68a699da9daec3929b79e6ac7
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-06-17 14:45:42 -0700

    imports prev code for graphs 04 k_min_moves (pre-mod)

--- algomon/units/04 graphs/04 k_min_moves.rs
+++ algomon/units/04 graphs/04 k_min_moves.rs
@@ -1,5 +1,44 @@
+use std::collections::HashSet;
+use std::collections::VecDeque;
 use std::error;
 use std::io;
+use std::str::FromStr;
+
+#[derive(Clone, Eq, Hash, PartialEq)]
+struct Coordinate {
+    r: usize,
+    c: usize,
+    val: i32,
+}
+
+fn build_Coordinate(r: i32, c: i32, val: i32) -> Coordinate {
+    Coordinate {
+        r: r,
+        c: c,
+        val: val,
+    }
+}
+
+fn get_neighbors(coord: Coordinate, grid: &Vec<Vec<i32>>) -> Vec<Coordinate> {
+    let mut neighbors: Vec<Coordinate> = vec![];
+    let deltaRow = [-1, 0, 1, 0];
+    let deltaCol = [0, 1, 0, -1];
+    for i in 0..deltaRow.len() {
+        let neighborRow = coord.r as i32 + deltaRow[i];
+        let neighborCol = coord.c as i32 + deltaCol[i];
+        if 0 <= neighborRow
+            && neighborRow < grid.len().try_into().unwrap()
+            && 0 <= neighborCol
+            && neighborCol < grid[0].len().try_into().unwrap()
+        {
+            if grid[neighborRow as usize][neighborCol as usize] == 1 {
+                neighbors.push(build_Coordinate(neighborRow, neighborCol, 1));
+            }
+        }
+    }
+
+    neighbors
+}
 
 fn get_knight_shortest_path(x: i32, y: i32) -> i32 {
     // WRITE YOUR BRILLIANT CODE HERE
