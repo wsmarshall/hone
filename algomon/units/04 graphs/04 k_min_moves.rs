9e5b9f681dc1ed4a995fc470d1d31131e9f75893
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-06-18 13:07:56 -0700

    gets graph 04 working (all tests pass)

--- algomon/units/04 graphs/04 k_min_moves.rs
+++ algomon/units/04 graphs/04 k_min_moves.rs
@@ -7,10 +7,15 @@ use std::io;
 struct Coordinate {
     r: i32,
     c: i32,
+    steps: i32,
 }
 
-fn build_Coordinate(r: i32, c: i32) -> Coordinate {
-    Coordinate { r: r, c: c }
+fn build_Coordinate(r: i32, c: i32, steps: i32) -> Coordinate {
+    Coordinate {
+        r: r,
+        c: c,
+        steps: steps,
+    }
 }
 
 fn get_neighbors(coord: &Coordinate) -> Vec<Coordinate> {
@@ -20,8 +25,8 @@ fn get_neighbors(coord: &Coordinate) -> Vec<Coordinate> {
     for i in 0..deltaRow.len() {
         let neighborRow = coord.r as i32 + deltaRow[i];
         let neighborCol = coord.c as i32 + deltaCol[i];
-
-        neighbors.push(build_Coordinate(neighborRow, neighborCol));
+        let distance = coord.steps + 1;
+        neighbors.push(build_Coordinate(neighborRow, neighborCol, distance));
     }
 
     neighbors
@@ -32,19 +37,21 @@ fn bfs(x: i32, y: i32) -> i32 {
     let mut queue: VecDeque<Coordinate> = VecDeque::new();
     let mut steps: i32 = 0;
 
-    queue.push_back(build_Coordinate(x, y));
+    queue.push_back(build_Coordinate(0, 0, 0));
 
     while !queue.is_empty() {
         if let Some(coord) = queue.pop_front() {
-            if coord.r == x && coord.c == y {
-                break;
-            }
+            if visited.insert(coord.clone()) {
+                if coord.r == x && coord.c == y {
+                    steps = coord.steps;
+                    return steps;
+                }
 
-            let neighbors = get_neighbors(&coord);
-            for i in neighbors {
-                queue.push_back(i);
+                let neighbors = get_neighbors(&coord);
+                for i in neighbors {
+                    queue.push_back(i);
+                }
             }
-            steps += 1;
         }
     }
 
