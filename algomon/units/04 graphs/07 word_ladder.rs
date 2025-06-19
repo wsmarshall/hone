3446c425f75e80577b5f1b250bb9d5c6cf2806dc
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-06-19 16:15:59 -0700

    imls and tests Word struct and get_neighbors

--- algomon/units/04 graphs/07 word_ladder.rs
+++ algomon/units/04 graphs/07 word_ladder.rs
@@ -3,35 +3,37 @@ use std::collections::VecDeque;
 use std::error;
 use std::io;
 
-#[derive(Clone, Eq, Hash, PartialEq)]
-struct Combination {
-    digits: Vec<i32>,
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+struct Word {
+    chars: Vec<char>,
     steps: i32,
 }
 
-fn build_Combination(digits: Vec<i32>, steps: i32) -> Combination {
-    Combination {
-        digits: digits.clone(),
+fn build_Word(chars: Vec<char>, steps: i32) -> Word {
+    Word {
+        chars: chars.clone(),
         steps: steps,
     }
 }
 
-fn get_neighbors(comb: &Combination) -> Vec<Combination> {
-    let mut neighbors: Vec<Combination> = vec![];
-    //up and down in sets of 2 by L to R digit place in the combination
-    for i in 0..4 {
-        let mut next_up = comb.digits.clone();
-        next_up[i] = (next_up[i] + 1) % 10;
+//takes in a word, gives back a list of words from the dictionary
+//that are one character away
+fn get_neighbors(word: &Word, dict: &Vec<&str>) -> Vec<Word> {
+    let mut neighbors: Vec<Word> = vec![];
 
-        let mut next_down = comb.digits.clone();
-        next_down[i] = (next_down[i] + 9) % 10; // handles wrap-around for -1
-
-        let distance = comb.steps + 1;
-
-        neighbors.push(build_Combination(next_up, distance));
-        neighbors.push(build_Combination(next_down, distance));
+    let chars = word.chars.clone(); //clone?
+    for i in dict {
+        let mut diffs = 0;
+        let mut temp: Vec<char> = i.chars().collect();
+        for j in 0..temp.len() {
+            if temp[j] != chars[j] {
+                diffs += 1;
+            }
+        }
+        if diffs == 1 {
+            neighbors.push(build_Word(i.chars().collect(), word.steps + 1));
+        }
     }
-
     neighbors
 }
 
