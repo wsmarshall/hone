e351662933580f1f13f4476a29d8c50209bd7038
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-06-19 16:36:46 -0700

    impls graph07 word_ladder (all tests pass)

--- algomon/units/04 graphs/07 word_ladder.rs
+++ algomon/units/04 graphs/07 word_ladder.rs
@@ -18,13 +18,13 @@ fn build_Word(chars: Vec<char>, steps: i32) -> Word {
 
 //takes in a word, gives back a list of words from the dictionary
 //that are one character away
-fn get_neighbors(word: &Word, dict: &Vec<&str>) -> Vec<Word> {
+fn get_neighbors(word: &Word, dict: &Vec<String>) -> Vec<Word> {
     let mut neighbors: Vec<Word> = vec![];
 
     let chars = word.chars.clone(); //clone?
     for i in dict {
         let mut diffs = 0;
-        let mut temp: Vec<char> = i.chars().collect();
+        let temp: Vec<char> = i.chars().collect();
         for j in 0..temp.len() {
             if temp[j] != chars[j] {
                 diffs += 1;
@@ -38,6 +38,26 @@ fn get_neighbors(word: &Word, dict: &Vec<&str>) -> Vec<Word> {
 }
 
 fn word_ladder(begin: String, end: String, word_list: Vec<String>) -> i32 {
-    // WRITE YOUR BRILLIANT CODE HERE
+    let mut queue: VecDeque<Word> = VecDeque::new();
+    let mut visited: HashSet<Vec<char>> = HashSet::new();
+
+    let start: Vec<char> = begin.chars().collect();
+    let target: Vec<char> = end.chars().collect();
+
+    queue.push_back(build_Word(start.clone(), 0));
+
+    while !queue.is_empty() {
+        if let Some(word) = queue.pop_front() {
+            if visited.insert(word.chars.clone()) {
+                if word.chars == target {
+                    return word.steps;
+                }
+                let neighbors = get_neighbors(&word, &word_list);
+                for i in neighbors {
+                    queue.push_back(i);
+                }
+            }
+        }
+    }
     0
 }
