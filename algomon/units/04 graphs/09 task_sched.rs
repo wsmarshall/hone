58f057388046343f8604f76a81c1b77adc2d3cfd
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-06-20 14:34:34 -0700

    starts impling graph09

---
+++ algomon/units/04 graphs/09 task_sched.rs
@@ -0,0 +1,10 @@
+use std::collections::HashMap;
+use std::collections::VecDeque;
+
+fn main() {
+    let tasks = vec!['a', 'b', 'c', 'd'];
+
+    let requirements = vec![vec!['a', 'b'], vec!['c', 'b'], vec!['b', 'd']];
+
+    println!("{:?}", num_steps(test));
+}
