9f6963b545f524ecb1ab7fbcea34af7534f81304
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-06-22 11:13:17 -0700

    adds imports and fn stubs to graph10

--- algomon/units/04 graphs/10 recon_seq.rs
+++ algomon/units/04 graphs/10 recon_seq.rs
@@ -1,6 +1,13 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::collections::VecDeque;
 use std::error;
 use std::io;
 use std::str::FromStr;
+
+fn get_indegree() -> i32 {}
+
+fn topo_sort() {}
 
 fn sequence_reconstruction(original: Vec<i32>, seqs: Vec<Vec<i32>>) -> bool {
     // WRITE YOUR BRILLIANT CODE HERE
