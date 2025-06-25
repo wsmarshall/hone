94ba2c471277673c5455311da529b95854c91a37
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-06-24 18:42:57 -0700

    continues graph10

--- algomon/units/04 graphs/10 recon_seq.rs
+++ algomon/units/04 graphs/10 recon_seq.rs
@@ -5,11 +5,53 @@ use std::error;
 use std::io;
 use std::str::FromStr;
 
-fn get_indegree() -> i32 {}
+fn assess_indegrees(graph: &mut HashMap<i32, i32>, seqs: &Vec<Vec<i32>>) {
+    for i in seqs {
+        graph.entry(i[0]).or_insert(0);
+        for j in 1..i.len() {
+            *graph.entry(i[j]).or_insert(0) += 1;
+        }
+    }
+}
 
-fn topo_sort() {}
+fn topo_sort(original: &Vec<i32>, seqs: &Vec<Vec<i32>>, map: HashMap<i32, HashSet<i32>>) -> bool {
+    let mut queue: VecDeque<i32> = VecDeque::new();
+    let mut indegrees: HashMap<i32, i32> = HashMap::new();
+    let mut recon = vec![];
+
+    assess_indegrees(&mut indegrees, &seqs);
+
+    for i in indegrees {
+        if indegrees.get(&i) == &0 {
+            queue.push_back(i);
+        }
+    }
+
+    while !queue.is_empty() {
+        if queue.size() > 1 {
+            return false; //no unique reconstruction
+        }
+        if let Some(num) = queue.pop_front() {
+            //add num to the reconstruction
+            recon.push(num);
+
+            //decrement neighbors num points to
+
+            //find any now 0 nodes and add them to queue
+        }
+    }
+
+    original == recon
+}
 
 fn sequence_reconstruction(original: Vec<i32>, seqs: Vec<Vec<i32>>) -> bool {
-    // WRITE YOUR BRILLIANT CODE HERE
-    false
+    let mut map: HashMap<i32, HashSet<i32>> = HashMap::new();
+    for i in seqs {
+        let first = i[0];
+        for j in 1..i.len() {
+            *map.entry(first).or_insert().insert(i[j]);
+        }
+    }
+
+    topo_sort(&original, &seqs, map)
 }
