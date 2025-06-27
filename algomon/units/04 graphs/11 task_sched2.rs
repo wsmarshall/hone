939031540b3d551ad3a5c7759195557e9f79358c
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-06-27 12:14:11 -0700

    writes first pass at graph11 (ownership issues with no String copy)

--- algomon/units/04 graphs/11 task_sched2.rs
+++ algomon/units/04 graphs/11 task_sched2.rs
@@ -5,70 +5,90 @@ use std::error;
 use std::io;
 use std::str::FromStr;
 
-fn assess_indegrees(graph: &mut HashMap<i32, i32>, map: &HashMap<String, i32>) {
-    //     println!("map: {:?}", map);
-    for (k, v) in map {
-        *graph.entry(*k).or_insert(0);
-        for j in v {
-            *graph.entry(*j).or_insert(0) += 1;
+fn assess_indegrees(reqs: &Vec<Vec<String>>, map: &mut HashMap<String, i32>) {
+    for i in reqs {
+        *map.entry(i[0]).or_insert(0);
+        for j in 1..i.len() {
+            *map.entry(i[j]).or_insert(0) += 1;
         }
     }
 }
 
-fn topo_sort(requirements: &Vec<Vec<String>>, map: HashMap<String, i32>) -> i32 {
-    let mut queue: VecDeque<i32> = VecDeque::new();
-    let mut indegrees: HashMap<i32, i32> = HashMap::new();
-    let mut recon = vec![];
+fn topo_sort(
+    requirements: &Vec<Vec<String>>,
+    map: HashMap<String, i32>,
+    edge_map: HashMap<String, String>,
+) -> i32 {
+    let mut queue: VecDeque<String> = VecDeque::new();
+    let mut indegrees: HashMap<String, i32> = HashMap::new();
+    let mut total_time: i32 = 0;
+    let mut visited: HashSet<String> = HashSet::new();
 
-    assess_indegrees(&mut indegrees, &requirements);
+    assess_indegrees(&requirements, &mut indegrees);
 
     for i in &indegrees {
         if *i.1 == 0 {
             queue.push_back(*i.0);
         }
     }
+    let mut max_time = 0;
+    for i in &queue {
+        visited.insert(i.clone());
+        let curr_time = *(map.get(i)).unwrap();
+        if curr_time > max_time {
+            max_time = curr_time;
+        }
+    }
+
+    total_time += max_time;
 
     //     println!("indegrees: {:?}", indegrees);
 
     while !queue.is_empty() {
-        if queue.len() > 1 {
-            return false; //no unique reconstruction
-        }
-        if let Some(num) = queue.pop_front() {
-            //add num to the reconstruction
-            recon.push(num);
+        if let Some(entry) = queue.pop_front() {
             //             println!("recon: {:?}", recon);
             //             println!("map: {:?}", map);
-            //access HashSet, decrement neighbors num points to
-            if map.contains_key(&num) {
-                let neighbors = map.get(&num).unwrap();
-                //                 println!("neighbors: {:?}", neighbors);
-                for i in neighbors.iter() {
-                    *indegrees.entry(*i).or_insert(0) -= 1;
-                }
+            //access indegrees, decrement
+            if edge_map.contains_key(&entry) {
+                let neighbor = edge_map.get(&entry).unwrap();
+                *(indegrees.get(neighbor)).unwrap() -= 1;
 
                 //find any now 0 nodes and add them to queue
                 //                 println!("indegrees: {:?}", indegrees);
                 for i in &indegrees {
                     if *i.1 == 0 {
-                        if !recon.contains(i.0) {
-                            //                             println!("new 0 entry: {:?}", i.0);
-                            queue.push_back(*i.0);
-                        }
+                        queue.push_back(*i.0);
                     }
                 }
             }
         }
-    }
 
-    *original == recon
+        max_time = 0;
+        for i in &queue {
+            if visited.insert(i.clone()) {
+                let curr_time = *(map.get(i)).unwrap();
+                if curr_time > max_time {
+                    max_time = curr_time;
+                }
+            }
+        }
+        total_time += max_time;
+    }
+    total_time
 }
 
 fn task_scheduling_2(tasks: Vec<String>, times: Vec<i32>, requirements: Vec<Vec<String>>) -> i32 {
-    let map:<String, i32> = HashMap::new();
+    let mut map: HashMap<String, i32> = HashMap::new(); //maps tasks to times
     for i in 0..tasks.len() {
         *map.entry(tasks[i]).or_insert(0) = times[i];
     }
-    
-    topo_sort(&requirements, map)
+
+    let mut edge_map: HashMap<String, String> = HashMap::new();
+    for i in &requirements {
+        for j in 0..i.len() - 1 {
+            edge_map.insert(i[j], i[j + 1]);
+        }
+    }
+
+    topo_sort(&requirements, map, edge_map)
 }
