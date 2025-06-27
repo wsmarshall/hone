e92b61bc4b90c8c551e67244a71358881d41fa0f
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-06-27 13:28:37 -0700

    fixes ownership issues (switches graph11 to ref based)

--- algomon/units/04 graphs/11 task_sched2.rs
+++ algomon/units/04 graphs/11 task_sched2.rs
@@ -5,24 +5,24 @@ use std::error;
 use std::io;
 use std::str::FromStr;
 
-fn assess_indegrees(reqs: &Vec<Vec<String>>, map: &mut HashMap<String, i32>) {
+fn assess_indegrees<'a>(reqs: &'a Vec<Vec<String>>, map: &mut HashMap<&'a String, i32>) {
     for i in reqs {
-        *map.entry(i[0]).or_insert(0);
+        *map.entry(&i[0]).or_insert(0);
         for j in 1..i.len() {
-            *map.entry(i[j]).or_insert(0) += 1;
+            *map.entry(&i[j]).or_insert(0) += 1;
         }
     }
 }
 
 fn topo_sort(
     requirements: &Vec<Vec<String>>,
-    map: HashMap<String, i32>,
-    edge_map: HashMap<String, String>,
+    map: HashMap<&String, i32>,
+    edge_map: HashMap<&String, &String>,
 ) -> i32 {
-    let mut queue: VecDeque<String> = VecDeque::new();
-    let mut indegrees: HashMap<String, i32> = HashMap::new();
+    let mut queue: VecDeque<&String> = VecDeque::new();
+    let mut indegrees: HashMap<&String, i32> = HashMap::new();
     let mut total_time: i32 = 0;
-    let mut visited: HashSet<String> = HashSet::new();
+    let mut visited: HashSet<&String> = HashSet::new();
 
     assess_indegrees(&requirements, &mut indegrees);
 
@@ -33,7 +33,7 @@ fn topo_sort(
     }
     let mut max_time = 0;
     for i in &queue {
-        visited.insert(i.clone());
+        visited.insert(i);
         let curr_time = *(map.get(i)).unwrap();
         if curr_time > max_time {
             max_time = curr_time;
@@ -51,7 +51,7 @@ fn topo_sort(
             //access indegrees, decrement
             if edge_map.contains_key(&entry) {
                 let neighbor = edge_map.get(&entry).unwrap();
-                *(indegrees.get(neighbor)).unwrap() -= 1;
+                indegrees.insert(neighbor, indegrees.get(neighbor).unwrap() - 1);
 
                 //find any now 0 nodes and add them to queue
                 //                 println!("indegrees: {:?}", indegrees);
@@ -65,7 +65,7 @@ fn topo_sort(
 
         max_time = 0;
         for i in &queue {
-            if visited.insert(i.clone()) {
+            if visited.insert(i) {
                 let curr_time = *(map.get(i)).unwrap();
                 if curr_time > max_time {
                     max_time = curr_time;
@@ -78,15 +78,15 @@ fn topo_sort(
 }
 
 fn task_scheduling_2(tasks: Vec<String>, times: Vec<i32>, requirements: Vec<Vec<String>>) -> i32 {
-    let mut map: HashMap<String, i32> = HashMap::new(); //maps tasks to times
+    let mut map: HashMap<&String, i32> = HashMap::new(); //maps tasks to times
     for i in 0..tasks.len() {
-        *map.entry(tasks[i]).or_insert(0) = times[i];
+        *map.entry(&tasks[i]).or_insert(0) = times[i];
     }
 
-    let mut edge_map: HashMap<String, String> = HashMap::new();
+    let mut edge_map: HashMap<&String, &String> = HashMap::new();
     for i in &requirements {
         for j in 0..i.len() - 1 {
-            edge_map.insert(i[j], i[j + 1]);
+            edge_map.insert(&i[j], &i[j + 1]);
         }
     }
 
