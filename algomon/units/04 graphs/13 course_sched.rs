d32739c38f1500ef010889c1b147d04472721bfc
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-06-27 15:17:56 -0700

    stubs and skells graph13 course_sched.rs

---
+++ algomon/units/04 graphs/13 course_sched.rs
@@ -0,0 +1,8 @@
+use std::error;
+use std::io;
+use std::str::FromStr;
+
+fn is_valid_course_schedule(n: i32, prerequisites: Vec<Vec<i32>>) -> bool {
+    // WRITE YOUR BRILLIANT CODE HERE
+    false
+}
