e059526852d79317470791a4abb9d7087f3d1f48
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-06-28 13:37:15 -0700

    stubs and starts PQ&H 00 (heap top 3)

---
+++ algomon/units/05 PQ & H/00 heap_fund.rs
@@ -0,0 +1,9 @@
+use std::error;
+use std::fmt::Display;
+use std::io;
+use std::str::FromStr;
+
+//fundamental exercise for PQ core fn'ality using Heap
+fn heap_top_3(arr: Vec<i32>) -> Vec<i32> {
+    Vec::new()
+}
