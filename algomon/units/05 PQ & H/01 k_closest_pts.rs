a3c7b942bc697389a6a63fc0f46104f3d2ca5fbc
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-06-28 14:57:36 -0700

    changes approach of pq&h 01 to HashMap, (all proper tests pass)

--- algomon/units/05 PQ & H/01 k_closest_pts.rs
+++ algomon/units/05 PQ & H/01 k_closest_pts.rs
@@ -1,35 +1,10 @@
-use std::cmp::Ordering;
 use std::cmp::Reverse;
 use std::collections::BinaryHeap;
+use std::collections::HashMap;
 use std::error;
 use std::fmt::Display;
 use std::io;
 use std::str::FromStr;
-
-#[derive(Eq, PartialEq)]
-struct Point {
-    x_y: Vec<i32>,
-    dist: i32,
-}
-
-fn build_Point(coords: &Vec<i32>, distance: i32) -> Point {
-    Point {
-        x_y: coords.clone(),
-        dist: distance,
-    }
-}
-
-impl Ord for Point {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.dist.cmp(&other.dist)
-    }
-}
-
-impl PartialOrd for Point {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
 
 fn distance(point: &Vec<i32>) -> i32 {
     let x = point[0];
@@ -40,16 +15,20 @@ fn distance(point: &Vec<i32>) -> i32 {
 }
 
 fn k_closest_points(points: Vec<Vec<i32>>, k: i32) -> Vec<Vec<i32>> {
-    let p_heap: BinaryHeap<Reverse<i32>> = BinaryHeap::new();
+    let mut p_heap: BinaryHeap<Reverse<i32>> = BinaryHeap::new();
+    //maps distances to Vec of a point's coordinates
+    let mut map: HashMap<i32, Vec<i32>> = HashMap::new();
     let mut ans: Vec<Vec<i32>> = vec![];
+
     for i in points {
         let d = distance(&i);
-        p_heap.push(Reverse(build_Point(&i, d)));
+        p_heap.push(Reverse(d));
+        map.insert(d, i.clone());
     }
 
-    for i in 0..k {
+    for _i in 0..k {
         if let Some(p) = p_heap.pop() {
-            ans.push(p.0.x_y);
+            ans.push(map.get(&(p.0)).unwrap().to_vec());
         }
     }
 
