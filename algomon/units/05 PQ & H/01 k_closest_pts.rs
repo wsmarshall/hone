ad3a7ca057b86bd4faa363ecfda607ec833a3039
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-06-28 13:47:07 -0700

    stubs and skells PQ&H 01: k_closest_pts

---
+++ algomon/units/05 PQ & H/01 k_closest_pts.rs
@@ -0,0 +1,9 @@
+use std::error;
+use std::fmt::Display;
+use std::io;
+use std::str::FromStr;
+
+fn k_closest_points(points: Vec<Vec<i32>>, k: i32) -> Vec<Vec<i32>> {
+    // WRITE YOUR BRILLIANT CODE HERE
+    Vec::new()
+}
