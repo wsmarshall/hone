613f4af898e99a8e11ff729354ba816222d18e13
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-06-28 13:46:07 -0700

    impls PQ&H 00 (passes all test)

--- algomon/units/05 PQ & H/00 heap_fund.rs
+++ algomon/units/05 PQ & H/00 heap_fund.rs
@@ -1,9 +1,23 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
 use std::error;
 use std::fmt::Display;
 use std::io;
 use std::str::FromStr;
 
-//fundamental exercise for PQ core fn'ality using Heap
 fn heap_top_3(arr: Vec<i32>) -> Vec<i32> {
-    Vec::new()
+    let mut heap: BinaryHeap<Reverse<i32>> = BinaryHeap::new();
+    let mut ans: Vec<i32> = vec![];
+
+    for i in arr {
+        heap.push(Reverse(i));
+    }
+
+    for i in 0..3 {
+        if let Some(num) = heap.pop() {
+            ans.push(num.0);
+        }
+    }
+
+    ans
 }
