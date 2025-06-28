1d18d70ff229f534e78b90488f98450eeb544610
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-06-28 13:50:28 -0700

    adds imports, first helper fn (so far) to PQ&H 01

--- algomon/units/05 PQ & H/01 k_closest_pts.rs
+++ algomon/units/05 PQ & H/01 k_closest_pts.rs
@@ -1,7 +1,11 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
 use std::error;
 use std::fmt::Display;
 use std::io;
 use std::str::FromStr;
+
+fn distance(p1: &Vec<i32>, p2: &Vec<i32>) -> i32 {}
 
 fn k_closest_points(points: Vec<Vec<i32>>, k: i32) -> Vec<Vec<i32>> {
     // WRITE YOUR BRILLIANT CODE HERE
