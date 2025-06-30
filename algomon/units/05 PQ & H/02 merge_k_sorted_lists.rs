2f4ee018b1d3f31a0a9a84cc870695c9a2710179
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-06-30 13:44:37 -0700

    fixes pq&h 02 skipping bug (all tests pass)

--- algomon/units/05 PQ & H/02 merge_k_sorted_lists.rs
+++ algomon/units/05 PQ & H/02 merge_k_sorted_lists.rs
@@ -5,7 +5,7 @@ use std::fmt::Display;
 use std::io;
 use std::str::FromStr;
 
-#[derive(Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq)]
 struct Item {
     val: i32,
     list: usize,
@@ -40,26 +40,19 @@ fn merge_k_sorted_lists(lists: Vec<Vec<i32>>) -> Vec<i32> {
     let mut ans: Vec<i32> = vec![];
     let mut k_heap: BinaryHeap<Item> = BinaryHeap::new();
 
-    //these 2 variables are for knowing when we're done
-    //with all the individual lists
-    let mut total_num = 0;
-    let mut current_count = 0;
-
     //adds the minimal element from each list to the minheap
     for i in 0..lists.len() {
-        total_num += lists[i].len();
-        let current = build_Item(lists[i][0], i, lists[i].len(), 1);
+        let current = build_Item(lists[i][0], i, lists[i].len(), 0);
         k_heap.push(current);
-        current_count += 1;
     }
 
-    while current_count < total_num {
+    while !k_heap.is_empty() {
+        //         println!("heap: {:?}", k_heap);
         if let Some(current_item) = k_heap.pop() {
             ans.push(current_item.val);
             let next_index = current_item.list_index + 1;
             if next_index < lists[current_item.list].len() {
                 let next_val = lists[current_item.list][next_index];
-
                 k_heap.push(build_Item(
                     next_val,
                     current_item.list,
@@ -67,8 +60,6 @@ fn merge_k_sorted_lists(lists: Vec<Vec<i32>>) -> Vec<i32> {
                     next_index,
                 ));
             }
-
-            current_count += 1;
         }
     }
 
