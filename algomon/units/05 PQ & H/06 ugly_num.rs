4c3375730fd50be21da4f8cc5067184b346cfd45
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-07-01 14:43:58 -0700

    impls pq&h 06

--- algomon/units/05 PQ & H/06 ugly_num.rs
+++ algomon/units/05 PQ & H/06 ugly_num.rs
@@ -1,7 +1,39 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+use std::collections::HashSet;
 use std::error;
 use std::io;
 
 fn nth_ugly_number(n: i32) -> i32 {
-    // WRITE YOUR BRILLIANT CODE HERE
+    let mut min_heap: BinaryHeap<Reverse<i32>> = BinaryHeap::new();
+    min_heap.push(Reverse(1));
+    let mut seen: HashSet<i32> = HashSet::new();
+
+    let mut count = 1;
+
+    while count < n {
+        if let Some(Reverse(num)) = min_heap.pop() {
+            let first = num * 2;
+            if seen.insert(first) {
+                min_heap.push(Reverse(first));
+            }
+
+            let second = num * 3;
+            if seen.insert(second) {
+                min_heap.push(Reverse(second));
+            }
+            let third = num * 5;
+            if seen.insert(third) {
+                min_heap.push(Reverse(third));
+            }
+            count += 1;
+        }
+        //         println!("state of the heap: {:?}", min_heap);
+    }
+
+    if let Some(Reverse(n)) = min_heap.pop() {
+        return n;
+    }
+
     0
 }
