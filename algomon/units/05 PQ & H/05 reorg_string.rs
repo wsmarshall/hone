d51e7bb61c98f57f26c444c5345109e923ed5d2d
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-07-01 12:27:01 -0700

    impls pq&h 05 (all tests pass)

--- algomon/units/05 PQ & H/05 reorg_string.rs
+++ algomon/units/05 PQ & H/05 reorg_string.rs
@@ -1,33 +1,5 @@
-use std::cmp::Ordering;
 use std::collections::BinaryHeap;
 use std::collections::HashMap;
-
-#[derive(Debug, Eq, PartialEq)]
-struct Item {
-    character: char,
-    val: usize,
-}
-
-fn build_Item(character: char, value: usize) -> Item {
-    Item {
-        character: character,
-        val: value,
-    }
-}
-
-impl PartialOrd<Item> for Item {
-    fn partial_cmp(&self, other: &Item) -> Option<Ordering> {
-        other.val.partial_cmp(&self.val) // - for MIN HEAP
-                                         //self.val.partial_cmp(&other.val)   // - for MAX HEAP
-    }
-}
-
-impl Ord for Item {
-    fn cmp(&self, other: &Self) -> Ordering {
-        other.val.cmp(&self.val) // - for MIN HEAP
-                                 //self.val.cmp(&other.val)           // - for MAX HEAP
-    }
-}
 
 impl Solution {
     pub fn reorganize_string(s: String) -> String {
@@ -37,31 +9,33 @@ impl Solution {
             *counts.entry(c).or_insert(0) += 1;
         }
 
-        let mut heap: BinaryHeap<Item> = BinaryHeap::new();
+        let mut heap = BinaryHeap::new();
         for (k, v) in &counts {
-            heap.push(build_Item(*k, *v as usize));
-        }
-
-        if let Some(max) = heap.peek() {
-            if max.val > (n + 1) / 2 {
-                return "".to_string();
-            }
+            heap.push((*v as usize, *k));
         }
 
         let mut collect: Vec<char> = Vec::with_capacity(n);
-        let mut start = 0;
-        while !heap.is_empty() {
-            if let Some(entry) = heap.pop() {
-                let mut count = 0;
-                while count < entry.val {
-                    collect.insert(start, entry.character);
-                    count += 1;
-                    start += 2;
-                    if start > n {
-                        start = 1;
-                    }
-                }
+
+        while heap.len() >= 2 {
+            let (freq1, char1) = heap.pop().unwrap();
+            let (freq2, char2) = heap.pop().unwrap();
+
+            collect.push(char1);
+            collect.push(char2);
+
+            if freq1 > 1 {
+                heap.push((freq1 - 1, char1));
             }
+            if freq2 > 1 {
+                heap.push((freq2 - 1, char2));
+            }
+        }
+
+        if let Some((freq, ch)) = heap.pop() {
+            if freq > 1 {
+                return "".to_string();
+            }
+            collect.push(ch);
         }
 
         let ans: String = collect.iter().collect();
