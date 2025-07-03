853b22633ed936e59c190514f2317ffa783815ac
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-07-03 15:10:49 -0700

    fixes (full understanding) of DP01 approach 3 (all tests pass)

--- algomon/units/06 DP/01 unique_paths3.rs
+++ algomon/units/06 DP/01 unique_paths3.rs
@@ -1,11 +1,12 @@
 use std::error;
 use std::io;
-//WHY IS THIS???
 
 fn unique_paths(m: i32, n: i32) -> i32 {
     let mut ans: i32 = 1;
-    for i in 1..m {
-        ans = ans * (n - 1 + i) / i;
+    let new_n = m + n - 2;
+    let k = m - 1;
+    for i in 1..=k {
+        ans = ans * (new_n + 1 - i) / i;
     }
     ans
 }
