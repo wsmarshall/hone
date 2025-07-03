8eded1a2cff2b1f60e4983c71214572cabeebbbc
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-07-03 12:15:43 -0700

    impls DP01, first approach

--- algomon/units/06 DP/01 unique_paths.rs
+++
@@ -1,9 +0,0 @@
-//part of the "grid DP" problem set
-
-use std::error;
-use std::io;
-
-fn unique_paths(m: i32, n: i32) -> i32 {
-    // WRITE YOUR BRILLIANT CODE HERE
-    0
-}
---
+++ algomon/units/06 DP/01 unique_paths1.rs
@@ -0,0 +1,20 @@
+//part of the "grid DP" problem set
+
+use std::error;
+use std::io;
+
+//takes "2D" approach
+fn unique_paths(m: i32, n: i32) -> i32 {
+    let mut grid: Vec<Vec<i32>> = vec![vec![1; n as usize]; m as usize];
+
+    let r = m as usize; //row
+    let c = n as usize; //column
+
+    for i in 1..r {
+        for j in 1..c {
+            grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+        }
+    }
+
+    grid[r - 1][c - 1]
+}
