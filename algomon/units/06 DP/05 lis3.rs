1cb67841a75555b250294c9a09249065591012ff
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-07-07 16:45:32 -0700

    impls DP05 3rd way (fastest) (all tests pass)

--- algomon/units/06 DP/05 lis3.rs
+++ algomon/units/06 DP/05 lis3.rs
@@ -3,7 +3,37 @@ use std::io;
 use std::str::FromStr;
 
 //for "hyperoptimized" approach (n log n vs n^2)
+
+fn bs(nums: &Vec<i32>) -> usize {
+    let mut left = 0;
+    let mut right = nums.len();
+
+    let mut ans = left;
+
+    while left < right {
+        let mid = left + (right - left) / 2;
+        if mid > ans {
+            ans = mid;
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    ans
+}
+
 fn longest_sub_len(nums: Vec<i32>) -> i32 {
-    // WRITE YOUR BRILLIANT CODE HERE
-    0
+    let mut seq: Vec<i32> = vec![];
+    for i in nums {
+        if Some(&i) > seq.last() {
+            seq.push(i);
+        } else if Some(&i) == seq.last() {
+            continue;
+        } else {
+            let greater = bs(&seq);
+            seq[greater] = i;
+        }
+    }
+    seq.len().try_into().unwrap()
 }
