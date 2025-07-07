f78017ca342b6e6dedfc78cfb6978f28cc3cde1b
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-07-07 11:59:28 -0700

    adds a file for a 3rd, "hyper-optimized" approach for DP 05

---
+++ algomon/units/06 DP/05 lis3.rs
@@ -0,0 +1,9 @@
+use std::error;
+use std::io;
+use std::str::FromStr;
+
+//for "hyperoptimized" approach (n log n vs n^2)
+fn longest_sub_len(nums: Vec<i32>) -> i32 {
+    // WRITE YOUR BRILLIANT CODE HERE
+    0
+}
