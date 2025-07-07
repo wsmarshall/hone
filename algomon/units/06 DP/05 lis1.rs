d2de547e4da41761e77db6135257cf64c2223d77
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-07-07 11:41:12 -0700

    splits DP05 into two for dift approaches

--- algomon/units/06 DP/05 lis1.rs
+++ algomon/units/06 DP/05 lis1.rs
@@ -2,6 +2,7 @@ use std::error;
 use std::io;
 use std::str::FromStr;
 
+//for bottom-up approach
 fn longest_sub_len(nums: Vec<i32>) -> i32 {
     // WRITE YOUR BRILLIANT CODE HERE
     0
---
+++ algomon/units/06 DP/05 lis2.rs
@@ -0,0 +1,9 @@
+use std::error;
+use std::io;
+use std::str::FromStr;
+
+//for top-down approach
+fn longest_sub_len(nums: Vec<i32>) -> i32 {
+    // WRITE YOUR BRILLIANT CODE HERE
+    0
+}
