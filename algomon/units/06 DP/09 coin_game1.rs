f5a1b5aaac87847f399dc9f6b90313ef065370ed
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-07-09 12:51:31 -0700

    starts on DP09-1

--- algomon/units/06 DP/09 coin_game1.rs
+++ algomon/units/06 DP/09 coin_game1.rs
@@ -1,9 +1,37 @@
+use std::cmp;
 use std::error;
 use std::io;
 use std::str::FromStr;
 
-//for "top down" approach
+fn top_down(memo: &mut Vec<i32>, prefix_sum: &Vec<i32>, l: usize, r: usize) -> i32 {
+    if l == r {
+        return prefix_sum[l];
+    }
+
+    if memo[l][r] != 0 {
+        return memo[l][r];
+    }
+
+    let sum = prefix_sum[r] - prefix_sum[l - 1];
+
+    let score = sum
+        - cmp::min(
+            top_down(memo, prefix_sum, l + 1, r),
+            top_down(memo, prefix_sum, l, r - 1),
+        );
+    memo[l][r] = score;
+
+    score
+}
+
 fn coin_game(coins: Vec<i32>) -> i32 {
-    // WRITE YOUR BRILLIANT CODE HERE
-    0
+    let n = coins.size();
+    let mut prefix_sum: Vec<i32> = vec![0; n + 1];
+    for i in 1..n {
+        prefix_sum[i] = prefix_sum[i - 1] + coins[i - 1];
+    }
+
+    let mut memo: Vec<i32> = vec![vec![0; n]; n];
+
+    top_down(&mut memo, &prefix_sum, 1, n)
 }
