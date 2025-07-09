91e4ae5fe3f034806cf72b111ad9aca5c0d9ae95
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-07-09 12:18:53 -0700

    splits DP09 into two for two approaches

---
+++ algomon/units/06 DP/09 coin_game1.rs
@@ -0,0 +1,9 @@
+use std::error;
+use std::io;
+use std::str::FromStr;
+
+//for "top down" approach
+fn coin_game(coins: Vec<i32>) -> i32 {
+    // WRITE YOUR BRILLIANT CODE HERE
+    0
+}
--- algomon/units/06 DP/09 coin_game2.rs
+++ algomon/units/06 DP/09 coin_game2.rs
@@ -2,6 +2,7 @@ use std::error;
 use std::io;
 use std::str::FromStr;
 
+//for "bottom up" approach
 fn coin_game(coins: Vec<i32>) -> i32 {
     // WRITE YOUR BRILLIANT CODE HERE
     0
