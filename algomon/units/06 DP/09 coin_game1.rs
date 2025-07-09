c20f3d9d037b16243cf6b5570587d7fe9d6a304f
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-07-09 16:29:36 -0700

    impls DP09-1 (all tests pass)

--- algomon/units/06 DP/09 coin_game1.rs
+++ algomon/units/06 DP/09 coin_game1.rs
@@ -4,34 +4,33 @@ use std::io;
 use std::str::FromStr;
 
 fn top_down(memo: &mut Vec<Vec<i32>>, prefix_sum: &Vec<i32>, l: usize, r: usize) -> i32 {
-    if l == r {
-        return prefix_sum[l];
-    }
-
     if memo[l][r] != 0 {
         return memo[l][r];
     }
 
     let sum = prefix_sum[r] - prefix_sum[l - 1];
 
-    let score = sum
-        - cmp::min(
-            top_down(memo, prefix_sum, l + 1, r),
-            top_down(memo, prefix_sum, l, r - 1),
-        );
-    memo[l][r] = score;
+    if l == r {
+        memo[l][r] = sum;
+    } else {
+        memo[l][r] = sum
+            - cmp::min(
+                top_down(memo, prefix_sum, l + 1, r),
+                top_down(memo, prefix_sum, l, r - 1),
+            );
+    }
 
-    score
+    memo[l][r]
 }
 
 fn coin_game(coins: Vec<i32>) -> i32 {
     let n = coins.len();
     let mut prefix_sum: Vec<i32> = vec![0; n + 1];
-    for i in 1..n {
+    for i in 1..=n {
         prefix_sum[i] = prefix_sum[i - 1] + coins[i - 1];
     }
 
-    let mut memo: Vec<Vec<i32>> = vec![vec![0; n]; n];
+    let mut memo: Vec<Vec<i32>> = vec![vec![0; n + 1]; n + 1];
 
     top_down(&mut memo, &prefix_sum, 1, n)
 }
