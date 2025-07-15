5bc1aee658e83c3b19e6c217a9f71dce392bd576
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-07-15 15:03:40 -0700

    impls DP12-1 (all tests pass)

--- algomon/units/06 DP/12 lcs1.rs
+++ algomon/units/06 DP/12 lcs1.rs
@@ -2,7 +2,29 @@ use std::cmp;
 use std::error;
 use std::io;
 
-fn lcs_dp(i: usize, j: usize, word1: &String, word2: &String, table: &mut Vec<Vec<i32>>) -> i32 {}
+//"top down" approach
+fn lcs_dp(i: usize, j: usize, word1: &String, word2: &String, table: &mut Vec<Vec<i32>>) -> i32 {
+    if i == 0 || j == 0 {
+        //base case
+        return 0;
+    }
+
+    if table[i][j] != -1 {
+        return table[i][j];
+    }
+
+    let res: i32;
+    if word1.as_bytes()[i - 1] == word2.as_bytes()[j - 1] {
+        res = lcs_dp(i - 1, j - 1, word1, word2, table) + 1;
+    } else {
+        res = cmp::max(
+            lcs_dp(i - 1, j, word1, word2, table),
+            lcs_dp(i, j - 1, word1, word2, table),
+        );
+    }
+    table[i][j] = res;
+    res
+}
 
 fn longest_common_subsequence(word1: String, word2: String) -> i32 {
     let m = word1.len();
