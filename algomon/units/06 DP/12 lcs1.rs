6d9837fd3d97b38403ce878fcb5613bba0148db7
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-07-15 14:54:50 -0700

    starts DP12-1

--- algomon/units/06 DP/12 lcs1.rs
+++ algomon/units/06 DP/12 lcs1.rs
@@ -1,8 +1,13 @@
+use std::cmp;
 use std::error;
 use std::io;
 
-//"top down"
+fn lcs_dp(i: usize, j: usize, word1: &String, word2: &String, table: &mut Vec<Vec<i32>>) -> i32 {}
+
 fn longest_common_subsequence(word1: String, word2: String) -> i32 {
-    // WRITE YOUR BRILLIANT CODE HERE
-    0
+    let m = word1.len();
+    let n = word2.len();
+    let mut table: Vec<Vec<i32>> = vec![vec![-1; n + 1]; m + 1];
+
+    lcs_dp(m, n, &word1, &word2, &mut table)
 }
