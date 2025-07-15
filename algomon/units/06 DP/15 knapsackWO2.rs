4447a8e7c42469b7a82268f17fc7cbdbb007f675
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-07-15 15:05:46 -0700

    stubs and skells for DP15 - wght-nly knpsck

---
+++ algomon/units/06 DP/15 knapsackWO2.rs
@@ -0,0 +1,9 @@
+use std::error;
+use std::fmt::Display;
+use std::io;
+use std::str::FromStr;
+
+fn knapsack_weight_only(weights: Vec<i32>) -> Vec<i32> {
+    // WRITE YOUR BRILLIANT CODE HERE
+    Vec::new()
+}
