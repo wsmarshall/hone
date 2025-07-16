a18de0922329f1b3eca7bd71e3786d68d31c858c
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-07-16 16:55:22 -0700

    continues DP15-1 (needs a refactor, TODO)

--- algomon/units/06 DP/15 knapsackWO1.rs
+++ algomon/units/06 DP/15 knapsackWO1.rs
@@ -1,15 +1,14 @@
-use std::collections::HashSet;
 use std::error;
 use std::fmt::Display;
 use std::io;
 use std::str::FromStr;
+use std::collections::HashSet;
 
-fn tddp(
-    n: usize,
-    weights: &Vec<i32>,
-    set: &mut HashSet<i32>,
-    memo: &mut Vec<Vec<bool>>,
-) -> Vec<i32> {
+fn tddp(n: usize, weights: &Vec<i32>, set: &mut HashSet<i32>, memo: &mut Vec<Vec<bool>>) -> Vec<i32> {
+    if n == 0 {
+        return Vec::from_iter(set.iter().cloned());
+    }
+    if memo()
     vec![]
 }
 
