a420131433455c362d81148f39cf440e72299475
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-07-16 16:36:01 -0700

    starts laying out structure of DP15-1

--- algomon/units/06 DP/15 knapsackWO1.rs
+++ algomon/units/06 DP/15 knapsackWO1.rs
@@ -1,9 +1,23 @@
+use std::collections::HashSet;
 use std::error;
 use std::fmt::Display;
 use std::io;
 use std::str::FromStr;
 
+fn tddp(
+    n: usize,
+    weights: &Vec<i32>,
+    set: &mut HashSet<i32>,
+    memo: &mut Vec<Vec<bool>>,
+) -> Vec<i32> {
+    vec![]
+}
+
 fn knapsack_weight_only(weights: Vec<i32>) -> Vec<i32> {
-    // WRITE YOUR BRILLIANT CODE HERE
-    Vec::new()
+    let mut set: HashSet<i32> = HashSet::new();
+    let m: usize = weights.len();
+    let n: usize = weights.iter().sum::<i32>() as usize;
+    let mut memo = vec![vec![false; n]; m];
+
+    tddp(m, &weights, &mut set, &mut memo)
 }
