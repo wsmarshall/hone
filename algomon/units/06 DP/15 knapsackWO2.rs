7d8646576b80bc3a2e93f676593d050934294c86
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-07-18 13:05:46 -0700

    starts DP15-2

--- algomon/units/06 DP/15 knapsackWO2.rs
+++ algomon/units/06 DP/15 knapsackWO2.rs
@@ -4,6 +4,12 @@ use std::io;
 use std::str::FromStr;
 
 fn knapsack_weight_only(weights: Vec<i32>) -> Vec<i32> {
-    // WRITE YOUR BRILLIANT CODE HERE
-    Vec::new()
+    let m: usize = weights.len();
+    let n: usize = weights.iter().sum::<i32>() as usize;
+    let mut table = vec![vec![false; n + 1]; m + 1]; //sum validity
+
+    table[0][0] = true;
+    for w in 1..=m {
+        for sum in 0..=n {}
+    }
 }
