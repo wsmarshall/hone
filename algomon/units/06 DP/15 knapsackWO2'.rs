f648fd75b8a87a42e6729a8d7c122d6bb2cc8490
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-07-18 13:45:09 -0700

    finishes small optimizantion tweak (space) for DP15-2

--- algomon/units/06 DP/15 knapsackWO2'.rs
+++ algomon/units/06 DP/15 knapsackWO2'.rs
@@ -7,22 +7,26 @@ use std::str::FromStr;
 fn knapsack_weight_only(weights: Vec<i32>) -> Vec<i32> {
     let m: usize = weights.len();
     let n: usize = weights.iter().sum::<i32>() as usize;
-    let mut table = vec![vec![false; n + 1]; m + 1]; //sum validity
+    let mut current = vec![false; n + 1]; //sum validity, first table
+    let mut prev = vec![false; n + 1]; //sum validity, second table
 
-    table[0][0] = true;
+    prev[0] = true;
     for i in 1..=m {
         for w in 0..=n {
-            table[i][w] = table[i][w] || table[i - 1][w];
+            current[w] = current[w] || prev[w];
 
             if w as i32 - weights[i - 1] >= 0 {
-                table[i][w] = table[i][w] || table[i - 1][w - weights[i - 1] as usize];
+                current[w] = current[w] || prev[w - weights[i - 1] as usize];
             }
         }
+        let temp = current;
+        current = prev;
+        prev = temp;
     }
-    // println!("table: {:?}", table);
+    // println!("current: {:?}, prev: {:?}", current, prev);
     let mut answers = vec![];
     for i in 0..=n {
-        if table[m][i] {
+        if prev[i] {
             answers.push(i as i32);
         }
     }
