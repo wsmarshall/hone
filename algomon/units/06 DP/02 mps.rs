ff8921e70d03179f773458abdf1e4f794e6cca9e
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-07-21 14:53:49 -0700

    starts DP02, (fills in base case)

--- algomon/units/06 DP/02 mps.rs
+++ algomon/units/06 DP/02 mps.rs
@@ -1,8 +1,24 @@
+use std::cmp;
 use std::error;
 use std::io;
 use std::str::FromStr;
 
 fn min_path_sum(grid: Vec<Vec<i32>>) -> i32 {
-    // WRITE YOUR BRILLIANT CODE HERE
-    0
+    let m = grid.len();
+    let n = grid[0].len();
+    let mut dp: Vec<Vec<i32>> = vec![vec![0; n]; m];
+
+    let mut sum = 0;
+    for i in 0..n {
+        //fill in first row
+        sum += grid[0][i];
+        dp[0][i] = sum;
+    }
+
+    sum = 0;
+    for i in 0..m {
+        //fill in first col
+        sum += grid[i][0];
+        dp[i][0] = sum;
+    }
 }
