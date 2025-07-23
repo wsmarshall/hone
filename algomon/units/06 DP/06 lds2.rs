15ac3f5b38c57cc3b96b149f7398159504c9445c
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-07-23 15:52:48 -0700

    starts filling in DP06-2

--- algomon/units/06 DP/06 lds2.rs
+++ algomon/units/06 DP/06 lds2.rs
@@ -1,8 +1,16 @@
+use std::cmp;
 use std::error;
 use std::io;
 use std::str::FromStr;
 
 fn find_largest_subset(nums: Vec<i32>) -> i32 {
-    // WRITE YOUR BRILLIANT CODE HERE
+    let n = nums.len();
+    let mut dp = vec![0; n + 1];
+
+    for i in 1..=n {
+        let current = nums[i - 1];
+        dp[i] = 1; //sets minimum case; subset of 1
+        for j in 1..i {}
+    }
     0
 }
