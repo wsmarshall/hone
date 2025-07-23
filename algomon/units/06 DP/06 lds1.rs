28fa712c9d11f22ba0b6e0efd4afe6430f0514b9
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-07-23 16:17:32 -0700

    starts fleshing out DP06-1

--- algomon/units/06 DP/06 lds1.rs
+++ algomon/units/06 DP/06 lds1.rs
@@ -1,8 +1,17 @@
+use std::cmp;
 use std::error;
 use std::io;
 use std::str::FromStr;
 
+fn dp(n: usize, nums: &Vec<i32>, table: &mut Vec<i32>) -> i32 {
+    if n == 0 {
+        return 0;
+    }
+}
+
 fn find_largest_subset(nums: Vec<i32>) -> i32 {
-    // WRITE YOUR BRILLIANT CODE HERE
-    0
+    let n = nums.len();
+    let mut table = vec![0; n + 1];
+
+    dp(n, &nums, &mut table)
 }
