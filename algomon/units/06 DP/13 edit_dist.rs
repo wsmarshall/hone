d4aba78d253b8da49834172e4a8fdc33a281d554
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-07-26 08:49:49 -0700

    starts DP13 coding using LCS approach

--- algomon/units/06 DP/13 edit_dist.rs
+++ algomon/units/06 DP/13 edit_dist.rs
@@ -1,7 +1,24 @@
 use std::error;
 use std::io;
 
+fn lcs(m: usize, n: usize, word1: &Vec<char>, word2: &Vec<char>, dp: &mut Vec<Vec<i32>>) -> i32 {
+    for 
+}
+
 fn min_distance(word1: String, word2: String) -> i32 {
-    // WRITE YOUR BRILLIANT CODE HERE
-    0
+    let m = word1.len();
+    let n = word2.len();
+    let word1: Vec<char> = word1.chars().collect();
+    let word2: Vec<char> = word2.chars().collect();
+    let mut dp: Vec<Vec<i32>> = vec![vec![0; n]; m];
+    let lcs = lcs(0, 0, &word1, &word2, &mut dp);
+    
+    let ans: i32;
+    if m > n {
+        ans = m as i32 - lcs;
+    } else {
+        ans = n as i32 - lcs;
+    }
+    
+    ans
 }
