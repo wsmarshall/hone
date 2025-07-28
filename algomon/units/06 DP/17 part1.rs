fd640d543b4d90171dfa6da0423a2b1f6fe9b5a0
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-07-28 16:54:07 -0700

    stubs DP17 with a brute force approach (passes all relevant tests, needs optimization to avoid test server stack overflow)

--- algomon/units/06 DP/17 part1.rs
+++ algomon/units/06 DP/17 part1.rs
@@ -2,7 +2,27 @@ use std::error;
 use std::io;
 use std::str::FromStr;
 
+fn target_exists(nums: &Vec<i32>, target: i32, current: i32, n: usize) -> bool {
+    if current == target {
+        return true;
+    }
+
+    if n == 0 || current > target {
+        return false;
+    }
+
+    let exists: bool = target_exists(nums, target, current + nums[n - 1], n - 1)
+        || target_exists(nums, target, current, n - 1);
+
+    exists
+}
+
 fn can_partition(nums: Vec<i32>) -> bool {
-    // WRITE YOUR BRILLIANT CODE HERE
-    false
+    let total: i32 = nums.iter().sum();
+    if total % 2 == 1 {
+        return false;
+    } else {
+        let target = total / 2;
+        return target_exists(&nums, target, 0, nums.len());
+    }
 }
