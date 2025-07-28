97d4f9b48aa9aa606757cbfa5fcbdafb8b278d06
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-07-28 14:53:58 -0700

    impls DP13-1 (all tests pass)

--- algomon/units/06 DP/13 edit_dist1.rs
+++ algomon/units/06 DP/13 edit_dist1.rs
@@ -8,10 +8,29 @@ fn min(a: i32, b: i32, c: i32) -> i32 {
 
 fn dfs(m: usize, n: usize, word1: &String, word2: &String, memo: &mut Vec<Vec<i32>>) -> i32 {
     if m == 0 {
-        return m;
+        return n as i32;
     } else if n == 0 {
-        return n;
+        return m as i32;
     }
+
+    if memo[m][n] != i32::MAX {
+        return memo[m][n];
+    }
+
+    let mut cost;
+    if word1.as_bytes()[m - 1] == word2.as_bytes()[n - 1] {
+        cost = 0;
+    } else {
+        cost = 1;
+    }
+
+    memo[m][n] = min(
+        dfs(m - 1, n - 1, word1, word2, memo) + cost,
+        dfs(m - 1, n, word1, word2, memo) + 1,
+        dfs(m, n - 1, word1, word2, memo) + 1,
+    );
+
+    memo[m][n]
 }
 
 fn min_distance(word1: String, word2: String) -> i32 {
