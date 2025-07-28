670d6379ea417ff963b2741a783aa91446d84a72
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-07-28 14:49:37 -0700

    starts setting up base cases for DP13-1

--- algomon/units/06 DP/13 edit_dist1.rs
+++ algomon/units/06 DP/13 edit_dist1.rs
@@ -6,20 +6,19 @@ fn min(a: i32, b: i32, c: i32) -> i32 {
     cmp::min(a, cmp::min(b, c))
 }
 
+fn dfs(m: usize, n: usize, word1: &String, word2: &String, memo: &mut Vec<Vec<i32>>) -> i32 {
+    if m == 0 {
+        return m;
+    } else if n == 0 {
+        return n;
+    }
+}
+
 fn min_distance(word1: String, word2: String) -> i32 {
     let m = word1.len();
     let n = word2.len();
-    let word1: Vec<char> = word1.chars().collect();
-    let word2: Vec<char> = word2.chars().collect();
-    let mut dp: Vec<Vec<i32>> = vec![vec![0; n + 1]; m + 1];
-    let lcs = lcs(&word1, &word2, &mut dp);
 
-    let ans: i32;
-    if m > n {
-        ans = m as i32 - lcs;
-    } else {
-        ans = n as i32 - lcs;
-    }
+    let mut memo: Vec<Vec<i32>> = vec![vec![i32::MAX; n + 1]; m + 1];
 
-    ans
+    dfs(m, n, &word1, &word2, &mut memo)
 }
