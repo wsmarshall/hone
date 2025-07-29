d8a96d7ad89a3e97e96c54cdac34eded80e2070f
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-07-29 13:02:19 -0700

    impls DP17-2 (all tests pass)

--- algomon/units/06 DP/17 part2.rs
+++ algomon/units/06 DP/17 part2.rs
@@ -2,46 +2,6 @@ use std::error;
 use std::io;
 use std::str::FromStr;
 
-fn target_exists(
-    nums: &Vec<i32>,
-    target: usize,
-    current: usize,
-    n: usize,
-    dp: &mut Vec<Vec<i32>>,
-) -> bool {
-    if current == target {
-        return true;
-    }
-
-    if n == 0 || current > target {
-        return false;
-    }
-
-    if dp[n][current] != -1 {
-        if dp[n][current] == 1 {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    let exists: bool = target_exists(
-        nums,
-        target,
-        current + <i32 as TryInto<usize>>::try_into(nums[n - 1]).unwrap(),
-        n - 1,
-        dp,
-    ) || target_exists(nums, target, current, n - 1, dp);
-
-    if exists {
-        dp[n][current] = 1;
-    } else {
-        dp[n][current] = 0;
-    }
-
-    exists
-}
-
 fn can_partition(nums: Vec<i32>) -> bool {
     let total: i32 = nums.iter().sum();
     if total % 2 == 1 {
@@ -57,20 +17,21 @@ fn can_partition(nums: Vec<i32>) -> bool {
             dp[i][0] = 1;
         }
 
-        for i in 0..=target as usize {
+        for i in 1..=target as usize {
             dp[0][i] = 0;
         }
 
         for i in 1..=n {
             for j in 1..=target as usize {
                 if dp[i - 1][j] == 1
-                    || (j as i32 - nums[i] >= 0 && dp[i][j - nums[i] as usize] == 1)
+                    || j <= nums[i - 1].try_into().unwrap()
+                        && (j as i32 - nums[i - 1] >= 0 && dp[i][j - nums[i - 1] as usize] == 1)
                 {
                     dp[i][j] = 1;
                 }
             }
         }
-
+        // println!("dp table: {:?}", dp);
         match dp[n][target as usize] {
             1 => return true,
             _ => return false,
