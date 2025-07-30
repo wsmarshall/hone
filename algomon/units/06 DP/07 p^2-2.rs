1293883c624579caef47b72713dc31f1a0b72419
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-07-30 16:31:18 -0700

    impls p^2-2 (anti-greeDY, per Jeff Erickson) (all tests pass)

--- algomon/units/06 DP/07 p^2-2.rs
+++ algomon/units/06 DP/07 p^2-2.rs
@@ -1,7 +1,21 @@
+use std::cmp;
 use std::error;
 use std::io;
 
 fn perfect_squares(n: i32) -> i32 {
-    // WRITE YOUR BRILLIANT CODE HERE
-    0
+    let length = n as usize;
+    let mut table = vec![i32::MAX; length + 1];
+    table[0] = 0;
+
+    let mut i: usize = 1;
+    let mut current: usize = i * i;
+    let target: usize = n as usize;
+    while current <= target {
+        for j in current..=length {
+            table[j] = cmp::min(table[j], table[j - current] + 1);
+        }
+        i += 1;
+        current = i * i;
+    }
+    table[length]
 }
