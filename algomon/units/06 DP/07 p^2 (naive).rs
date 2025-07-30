8c671ffcfb6436327bdb0e1cc4fb13dc00d67e39
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-07-30 13:45:00 -0700

    impls DP07-naive (should all tests pass?)

--- algomon/units/06 DP/07 p^2 (naive).rs
+++ algomon/units/06 DP/07 p^2 (naive).rs
@@ -2,6 +2,21 @@ use std::error;
 use std::io;
 
 fn perfect_squares(n: i32) -> i32 {
-    // WRITE YOUR BRILLIANT CODE HERE
-    0
+    let mut total: usize = n.try_into().unwrap();
+    let mut count: i32 = 0;
+    let root: usize = ((n as f64).sqrt().floor()) as usize;
+
+    let mut i = root;
+    while i > 0 {
+        // println!("i: {}", i);
+        let current = i * i;
+        while (total as i32 - current as i32) >= 0 {
+            count += 1;
+            // println!("total: {}", total);
+            total -= current;
+        }
+        i -= 1;
+    }
+
+    count
 }
