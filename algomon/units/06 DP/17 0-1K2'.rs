c2f1f2bc7add054f637ddd5938743e649439cba2
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-08-01 16:38:17 -0700

    fixes closing delimiters bugs, all tests pass for DP17-02'

--- algomon/units/06 DP/17 0-1K2'.rs
+++ algomon/units/06 DP/17 0-1K2'.rs
@@ -1,8 +1,29 @@
+use std::cmp;
 use std::error;
 use std::io;
 use std::str::FromStr;
 
 fn knapsack(weights: Vec<i32>, values: Vec<i32>, max_weight: i32) -> i32 {
-    // WRITE YOUR BRILLIANT CODE HERE
-    0
+    let mut table: Vec<i32> = vec![-1; (max_weight as usize) + 1];
+
+    table[0] = 0; //base case
+
+    for i in 0..weights.len() {
+        let mut j: i32 = max_weight;
+        while j >= weights[i] {
+            if table[(j - weights[i]) as usize] != -1 {
+                table[j as usize] = cmp::max(
+                    table[j as usize],
+                    table[(j - weights[i]) as usize] + values[i],
+                );
+            }
+
+            j -= 1;
+        }
+    }
+    if let Some(num) = table.iter().max() {
+        return *num;
+    } else {
+        return 0;
+    }
 }
