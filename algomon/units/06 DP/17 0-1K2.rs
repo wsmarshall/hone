725945e173a4616a956238b2acaf7650aca005d3
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-08-01 15:43:38 -0700

    tweaks dP17-1 such that memoization actually happens

--- algomon/units/06 DP/17 0-1K2.rs
+++ algomon/units/06 DP/17 0-1K2.rs
@@ -3,24 +3,35 @@ use std::error;
 use std::io;
 use std::str::FromStr;
 
-fn knapsack(weights: Vec<i32>, values: Vec<i32>, max_weight: i32) -> i32 {
-    let n = weights.len(); //number of items
-    let cap: usize = max_weight.try_into().unwrap();
-
-    let mut table: Vec<Vec<i32>> = vec![vec![0; cap + 1]; n + 1];
-
-    for i in 1..=n {
-        for j in 1..=cap {
-            if (j as i32) < weights[i - 1] {
-                table[i][j] = table[i - 1][j];
-            } else {
-                table[i][j] = cmp::max(
-                    table[i - 1][j],
-                    table[i - 1][j - weights[i - 1] as usize] + values[i - 1],
-                );
-            }
-        }
+fn dfs(
+    weights: &Vec<i32>,
+    values: &Vec<i32>,
+    n: usize,
+    cap: i32,
+    table: &mut Vec<Vec<i32>>,
+) -> i32 {
+    if n == 0 || cap <= 0 {
+        return 0;
     }
 
-    table[n][cap]
+    if table[n][cap as usize] != -1 {
+        return table[n][cap as usize];
+    }
+
+    if weights[n - 1] > cap {
+        return dfs(weights, values, n - 1, cap, table);
+    } else {
+        let with = dfs(weights, values, n - 1, cap - weights[n - 1], table) + values[n - 1];
+        let without = dfs(weights, values, n - 1, cap, table);
+        table[n][cap as usize] = cmp::max(with, without);
+        return table[n][cap as usize];
+    }
+}
+
+fn knapsack(weights: Vec<i32>, values: Vec<i32>, max_weight: i32) -> i32 {
+    let n = weights.len();
+    let cap: usize = max_weight.try_into().unwrap();
+    let mut table: Vec<Vec<i32>> = vec![vec![-1; cap + 1]; n + 1];
+
+    dfs(&weights, &values, n, max_weight, &mut table)
 }
