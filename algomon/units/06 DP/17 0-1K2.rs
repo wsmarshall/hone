9357ada3c6457d35ac7c76c5c5e861b9294d0cc3
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-08-01 14:03:56 -0700

    fully impls DP17-02 (all tests pass)

--- algomon/units/06 DP/17 0-1K2.rs
+++ algomon/units/06 DP/17 0-1K2.rs
@@ -5,19 +5,19 @@ use std::str::FromStr;
 
 fn knapsack(weights: Vec<i32>, values: Vec<i32>, max_weight: i32) -> i32 {
     let n = weights.len(); //number of items
-    let cap: usize = weights.iter().sum::<i32>().try_into().unwrap(); //total weight of all items
+    let cap: usize = max_weight.try_into().unwrap();
 
     let mut table: Vec<Vec<i32>> = vec![vec![0; cap + 1]; n + 1];
 
     for i in 1..=n {
         for j in 1..=cap {
-            if table[i][j] + weights[i - 1] < max_weight && j as i32 - weights[i - 1] >= 0 {
+            if (j as i32) < weights[i - 1] {
+                table[i][j] = table[i - 1][j];
+            } else {
                 table[i][j] = cmp::max(
                     table[i - 1][j],
                     table[i - 1][j - weights[i - 1] as usize] + values[i - 1],
                 );
-            } else {
-                table[i][j] = table[i - 1][j];
             }
         }
     }
