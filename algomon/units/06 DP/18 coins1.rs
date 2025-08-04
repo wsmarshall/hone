30dfb1beb012bf98d980b3db00246592df6f476f
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-08-04 15:46:40 -0700

    impls naive soln for DP18-1 (optimize next)

--- algomon/units/06 DP/18 coins1.rs
+++ algomon/units/06 DP/18 coins1.rs
@@ -1,8 +1,34 @@
+use std::cmp;
 use std::error;
 use std::io;
 use std::str::FromStr;
 
+fn min_coins(coins: &Vec<i32>, amount: i32, sum: i32) -> i32 {
+    if sum == amount {
+        return 0;
+    }
+
+    if sum > amount {
+        return i32::MAX;
+    }
+
+    let mut ans = i32::MAX;
+    for coin in coins {
+        let result = min_coins(coins, amount, sum + coin);
+        if result == i32::MAX {
+            continue;
+        }
+        ans = cmp::min(ans, result + 1);
+    }
+
+    return ans;
+}
+
 fn coin_change(coins: Vec<i32>, amount: i32) -> i32 {
-    // WRITE YOUR BRILLIANT CODE HERE
-    0
+    let result = min_coins(&coins, amount, 0);
+    if result == i32::MAX {
+        return -1;
+    } else {
+        return result;
+    }
 }
