6bf9310bf1df93f9b4f7833920f3d11712345af0
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-08-05 16:46:00 -0700

    impls DP08 (all tests pass)

--- algomon/units/06 DP/08 div_game.rs
+++ algomon/units/06 DP/08 div_game.rs
@@ -2,6 +2,17 @@ use std::error;
 use std::io;
 
 fn divisor_game(n: i32) -> bool {
-    // WRITE YOUR BRILLIANT CODE HERE
-    false
+    let n: usize = n.try_into().unwrap();
+    let mut table: Vec<bool> = vec![false; n + 1];
+
+    for i in 2..=n {
+        for j in 1..i {
+            if i % j == 0 && !table[i - j] {
+                table[i] = true;
+                break;
+            }
+        }
+    }
+
+    table[n]
 }
