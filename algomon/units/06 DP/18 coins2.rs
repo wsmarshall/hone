2e8564cac95349ae60432cdee736ce90f840d4d5
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-08-04 17:14:18 -0700

    starts DP18-2

--- algomon/units/06 DP/18 coins2.rs
+++ algomon/units/06 DP/18 coins2.rs
@@ -1,8 +1,25 @@
+use std::cmp;
 use std::error;
 use std::io;
 use std::str::FromStr;
 
 fn coin_change(coins: Vec<i32>, amount: i32) -> i32 {
-    // WRITE YOUR BRILLIANT CODE HERE
-    0
+    if amount == 0 {
+        return 0;
+    }
+
+    let amt: usize = <i32 as TryInto<usize>>::try_into(amount).unwrap();
+    let mut memo: Vec<i32> = vec![i32::MAX - 10; amt + 1];
+    memo[0] = 0;
+    for i in 1..=memo.len() {
+        for j in 0..coins.len() {
+            if coins[j] <= i.try_into().unwrap() {
+                memo[i] = cmp::min(
+                    memo[i],
+                    memo[i - <i32 as TryInto<usize>>::try_into(coins[j]).unwrap()] + 1,
+                );
+            }
+        }
+    }
+    memo[amt]
 }
