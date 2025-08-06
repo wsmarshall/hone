8f882de28b4eb8fe5f4839c0470f229c0d4cdda5
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-08-05 18:11:31 -0700

    impls DP19-2 and DP19-2' (all tests pass)

--- algomon/units/06 DP/19 change2.rs
+++ algomon/units/06 DP/19 change2.rs
@@ -3,6 +3,20 @@ use std::io;
 use std::str::FromStr;
 
 fn coin_game(coins: Vec<i32>, amount: i32) -> i32 {
-    // WRITE YOUR BRILLIANT CODE HERE
-    0
+    let m = coins.len();
+    let n: usize = amount.try_into().unwrap();
+    let mut table: Vec<Vec<i32>> = vec![vec![0; n + 1]; m + 1];
+
+    table[0][0] = 1;
+
+    for i in 1..=m {
+        for j in 0..=n {
+            table[i][j] += table[i - 1][j];
+            if j as i32 >= coins[i - 1] {
+                table[i][j] += table[i][j - coins[i - 1] as usize];
+            }
+        }
+    }
+
+    table[m][n]
 }
