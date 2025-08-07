3dc097498d5f0540959279023e95591f143c2c54
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-08-07 14:52:44 -0700

    stores first attempt at DSU1 for posterity (MAJOR refactor necessary)

--- algomon/units/07 AdvDS/01 DSU.rs
+++ algomon/units/07 AdvDS/01 DSU.rs
@@ -1,12 +1,14 @@
 //Java since Rust is not Implemented
 
-public static class SameSet {
+    public static class SameSet {
         public void merge(int x, int y) {
             // WRITE YOUR BRILLIANT CODE HERE
         }
 
         public boolean isSame(int x, int y) {
-            // AND HERE
+            if (x == y) {
+                return true;
+            }
             return false;
         }
     }
