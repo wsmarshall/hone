d800362d0231702b09258f72470bd80eade1e0a1
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-08-07 14:49:43 -0700

    skells DSU1 in Java since Rust not implemented

---
+++ algomon/units/07 AdvDS/01 DSU.rs
@@ -0,0 +1,12 @@
+//Java since Rust is not Implemented
+
+public static class SameSet {
+        public void merge(int x, int y) {
+            // WRITE YOUR BRILLIANT CODE HERE
+        }
+
+        public boolean isSame(int x, int y) {
+            // AND HERE
+            return false;
+        }
+    }
