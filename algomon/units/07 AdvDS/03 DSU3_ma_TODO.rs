c877f552ab38d01465879e765dddfaf50adf2ab7
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-08-11 14:18:52 -0700

    adds java DSU3 and stubs autoc(trie1)

--- algomon/units/07 AdvDS/03 DSU3_ma_TODO.rs
+++ algomon/units/07 AdvDS/03 DSU3_ma_TODO.rs
@@ -4,5 +4,9 @@ use std::io;
 
 fn merge_accounts(accounts: Vec<Vec<String>>) -> Vec<Vec<String>> {
     // WRITE YOUR BRILLIANT CODE HERE
-    Vec::new()
+    Vec::new();
+
+    let x = 6;
+    x = 5;
+    const x = 87;
 }
