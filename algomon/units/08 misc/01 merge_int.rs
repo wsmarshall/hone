80644831bd4bf8929cd74248fa23a516901132bb
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-08-18 13:20:38 -0700

    stubs and skells first file for new misc section

---
+++ algomon/units/08 misc/01 merge_int.rs
@@ -0,0 +1,9 @@
+use std::error;
+use std::fmt::Display;
+use std::io;
+use std::str::FromStr;
+
+fn merge_intervals(intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    // WRITE YOUR BRILLIANT CODE HERE
+    Vec::new()
+}
