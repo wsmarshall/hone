cdf563667cbf4279ff024e61e2090e9160192226
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-08-23 12:19:31 -0700

    starts working on O(n) approach for MSC02

--- algomon/units/08 MSC/02 insert_int.rs
+++ algomon/units/08 MSC/02 insert_int.rs
@@ -1,1 +1,8 @@
+fn insert_interval(mut intervals: Vec<Vec<i32>>, new_interval: Vec<i32>) -> Vec<Vec<i32>> {
+    let mut answers = vec![];
 
+    let mut i = 0;
+    let n = intervals.len();
+
+    answers
+}
