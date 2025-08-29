bc241f4049a3e036df0e43b07f3996bbe5cf7289
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-08-29 14:53:44 -0700

    starts working on the O(n) approach for MSC02

--- algomon/units/08 MSC/02 insert_int.rs
+++ algomon/units/08 MSC/02 insert_int.rs
@@ -1,8 +1,28 @@
+use std::cmp;
+use std::error;
+use std::fmt::Display;
+use std::io;
+use std::str::FromStr;
+
 fn insert_interval(mut intervals: Vec<Vec<i32>>, new_interval: Vec<i32>) -> Vec<Vec<i32>> {
-    let mut answers = vec![];
+    let mut answer: Vec<Vec<i32>> = vec![];
+    let mut new_used = false;
 
-    let mut i = 0;
-    let n = intervals.len();
+    for i in intervals {
+        if !new_used {
+            if i[1] <= new_interval[0] {
+                answer.push(i);
+            } else if i[0] < new_interval[1] || i[1] > new_interval[0] {
+                answer.push(vec![
+                    cmp::min(i[0], new_interval[0]),
+                    cmp::max(i[1], new_interval[1]),
+                ]);
+                new_used = true;
+            }
+        } else {
+            answer.push(i);
+        }
+    }
 
-    answers
+    answer
 }
