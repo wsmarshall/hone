1dca798bcda1a57dc1f10af64d9eaf7ed3961607
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-09-01 12:22:51 -0700

    starts trying to bugfix MSC02 - no output

--- algomon/units/08 MSC/02 insert_int.rs
+++ algomon/units/08 MSC/02 insert_int.rs
@@ -4,19 +4,20 @@ use std::fmt::Display;
 use std::io;
 use std::str::FromStr;
 
-fn insert_interval(mut intervals: Vec<Vec<i32>>, new_interval: Vec<i32>) -> Vec<Vec<i32>> {
+fn insert_interval(intervals: Vec<Vec<i32>>, new_interval: Vec<i32>) -> Vec<Vec<i32>> {
     let mut answer: Vec<Vec<i32>> = vec![];
     let mut new_used = false;
+    let mut new_pot = new_interval.clone();
 
     for i in intervals {
         if !new_used {
-            if i[1] <= new_interval[0] {
+            if i[1] < new_pot[0] {
                 answer.push(i);
-            } else if i[0] < new_interval[1] || i[1] > new_interval[0] {
-                answer.push(vec![
-                    cmp::min(i[0], new_interval[0]),
-                    cmp::max(i[1], new_interval[1]),
-                ]);
+            } else if i[1] >= new_pot[0] || i[0] <= new_pot[1] {
+                new_pot = (vec![cmp::min(i[0], new_pot[0]), cmp::max(i[1], new_pot[1])]);
+            } else if i[0] <= new_pot[1] && !new_used {
+                answer.push(new_pot.clone());
+                answer.push(i);
                 new_used = true;
             }
         } else {
