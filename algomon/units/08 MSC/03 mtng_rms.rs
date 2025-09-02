a5307d064e16ebf92f9afe8feee9dbff858e5b5b
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-09-02 12:16:09 -0700

    impls MSC03 (all tests pass)

--- algomon/units/08 MSC/03 mtng_rms.rs
+++ algomon/units/08 MSC/03 mtng_rms.rs
@@ -3,6 +3,12 @@ use std::io;
 use std::str::FromStr;
 
 fn meeting_rooms(intervals: Vec<Vec<i32>>) -> bool {
-    // WRITE YOUR BRILLIANT CODE HERE
-    false
+    let mut meetings = intervals.clone();
+    meetings.sort();
+    for i in 1..meetings.len() {
+        if meetings[i][0] < meetings[i - 1][1] {
+            return false;
+        }
+    }
+    true
 }
