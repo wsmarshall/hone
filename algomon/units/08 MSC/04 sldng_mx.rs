f5d2cfbdf950c359ad133b2e3e797b764e054866
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-09-06 13:40:52 -0700

    starts sketching out monotonic stack approach for  MSC04

--- algomon/units/08 MSC/04 sldng_mx.rs
+++ algomon/units/08 MSC/04 sldng_mx.rs
@@ -1,9 +1,25 @@
+use std::collections::VecDeque;
 use std::error;
 use std::fmt::Display;
 use std::io;
 use std::str::FromStr;
 
 fn sliding_window_maximum(nums: Vec<i32>, k: i32) -> Vec<i32> {
-    // WRITE YOUR BRILLIANT CODE HERE
-    Vec::new()
+    let mut maxes: Vec<i32> = vec![];
+
+    //monotonically decreasing "stack"
+    let mut track: VecDeque<(usize, i32)> = VecDeque::new();
+
+    for (i, el) in nums.iter().enumerate() {
+        while !track.is_empty() && track.back() <= el {
+            track.pop_back();
+        }
+        track.push_back((i, el));
+
+        if track.front() == i - k {
+            track.pop_front();
+        }
+    }
+
+    maxes
 }
