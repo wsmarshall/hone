4d1f658d0f3fb603b417d5852c643d7e3951c9f8
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-09-08 14:08:34 -0700

    skells MSC05 for future approach

---
+++ algomon/units/08 MSC/05 dl_tmps.rs
@@ -0,0 +1,9 @@
+use std::error;
+use std::fmt::Display;
+use std::io;
+use std::str::FromStr;
+
+fn daily_temperatures(t: Vec<i32>) -> Vec<i32> {
+    // WRITE YOUR BRILLIANT CODE HERE
+    Vec::new()
+}
