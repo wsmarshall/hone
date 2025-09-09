767b05d061a9e332445cabc821e8f30b05423d97
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-09-09 13:59:03 -0700

    writes first pass at MSC05 (compiles, runs, bugfixes TODO)

--- algomon/units/08 MSC/05 dl_tmps.rs
+++ algomon/units/08 MSC/05 dl_tmps.rs
@@ -1,9 +1,23 @@
+use std::collections::VecDeque;
 use std::error;
 use std::fmt::Display;
 use std::io;
 use std::str::FromStr;
 
 fn daily_temperatures(t: Vec<i32>) -> Vec<i32> {
-    // WRITE YOUR BRILLIANT CODE HERE
-    Vec::new()
+    let mut results: Vec<i32> = Vec::with_capacity(t.len());
+
+    let mut stack: VecDeque<(usize, i32)> = VecDeque::new();
+
+    stack.push_back((0usize, t[0]));
+
+    for (i, el) in t.iter().skip(1).enumerate() {
+        while !stack.is_empty() && el > &stack.back().unwrap().1 {
+            let current = stack.pop_back().unwrap();
+            results.push((i - current.0).try_into().unwrap());
+        }
+        stack.push_back((i, *el));
+    }
+
+    results
 }
