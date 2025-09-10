18651ec0293a861cf053c2dbfcf5037493b39ddb
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-09-10 13:07:06 -0700

    addresses MSC05 vec len bug

--- algomon/units/08 MSC/05 dl_tmps.rs
+++ algomon/units/08 MSC/05 dl_tmps.rs
@@ -5,7 +5,7 @@ use std::io;
 use std::str::FromStr;
 
 fn daily_temperatures(t: Vec<i32>) -> Vec<i32> {
-    let mut results: Vec<i32> = Vec::with_capacity(t.len());
+    let mut results: Vec<i32> = vec![0; t.len()];
 
     let mut stack: VecDeque<(usize, i32)> = VecDeque::new();
 
@@ -14,7 +14,7 @@ fn daily_temperatures(t: Vec<i32>) -> Vec<i32> {
     for (i, el) in t.iter().skip(1).enumerate() {
         while !stack.is_empty() && el > &stack.back().unwrap().1 {
             let current = stack.pop_back().unwrap();
-            results.push((i - current.0).try_into().unwrap());
+            results[current.0] = ((i - current.0).try_into().unwrap());
         }
         stack.push_back((i, *el));
     }
