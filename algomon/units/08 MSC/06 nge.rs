5983630be032fc1435265dc11760be251c6fa5d9
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-09-17 11:39:35 -0700

    bugfixes MSC06 (array instn instead of vec! macro call)

--- algomon/units/08 MSC/06 nge.rs
+++ algomon/units/08 MSC/06 nge.rs
@@ -5,13 +5,13 @@ use std::io;
 use std::str::FromStr;
 
 fn next_greater_elements(nums: Vec<i32>) -> Vec<i32> {
-    let mut results: Vec<i32> = [-1; nums.len()];
+    let mut results: Vec<i32> = vec![-1; nums.len()];
     let mut mstack: VecDeque<(usize, i32)> = VecDeque::new();
 
     for (i, el) in nums.iter().enumerate() {
         while !mstack.is_empty() && el > &mstack.back().unwrap().1 {
             let current = mstack.pop_back().unwrap();
-            results[current.0] = el;
+            results[current.0] = *el;
         }
         mstack.push_back((i, *el));
     }
