827e8bcd280eddbc9dd20e6cccc3d01088117c53
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-11-20 12:37:05 -0800

    iterates on k closest pts in pq/h section (drops use of HashMap)

--- algomon/units/05 PQ & H/01 k_closest_pts.rs
+++ algomon/units/05 PQ & H/01 k_closest_pts.rs
@@ -1,35 +1,28 @@
 use std::cmp::Reverse;
 use std::collections::BinaryHeap;
-use std::collections::HashMap;
 use std::error;
 use std::fmt::Display;
 use std::io;
 use std::str::FromStr;
 
+//this assumes any input has only 2 i32 entries
+//note that this return value should always be positive
 fn distance(point: &Vec<i32>) -> i32 {
     let x = point[0];
     let y = point[1];
-    let dist: i32 = (x * x + y * y);
-
-    dist
+    ((x * x) + (y * y))
 }
 
 fn k_closest_points(points: Vec<Vec<i32>>, k: i32) -> Vec<Vec<i32>> {
-    let mut p_heap: BinaryHeap<Reverse<i32>> = BinaryHeap::new();
-    //maps distances to Vec of a point's coordinates
-    let mut map: HashMap<i32, Vec<i32>> = HashMap::new();
+    let mut min_heap: BinaryHeap<Reverse<(i32, Vec<i32>)>> = BinaryHeap::new();
     let mut ans: Vec<Vec<i32>> = vec![];
-
-    for i in points {
-        let d = distance(&i);
-        p_heap.push(Reverse(d));
-        map.insert(d, i.clone());
+    for p in points {
+        let d: i32 = distance(&p);
+        min_heap.push(Reverse((d, p.clone())));
     }
 
     for _i in 0..k {
-        if let Some(p) = p_heap.pop() {
-            ans.push(map.get(&(p.0)).unwrap().to_vec());
-        }
+        ans.push(min_heap.pop().unwrap().0 .1.to_vec());
     }
 
     ans
