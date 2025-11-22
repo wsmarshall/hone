e73a6dc667fbb467ff8826a7fc26a987237f2921
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-11-21 16:14:00 -0800

    updates unique paths (DP01)

--- algomon/units/06 DP/01 unique_paths1.rs
+++ algomon/units/06 DP/01 unique_paths1.rs
@@ -5,16 +5,13 @@ use std::io;
 
 //takes "2D" approach
 fn unique_paths(m: i32, n: i32) -> i32 {
-    let mut grid: Vec<Vec<i32>> = vec![vec![1; n as usize]; m as usize];
-
-    let r = m as usize; //row
-    let c = n as usize; //column
-
-    for i in 1..r {
-        for j in 1..c {
+    let rows: usize = m.try_into().unwrap();
+    let cols: usize = n.try_into().unwrap();
+    let mut grid = vec![vec![1; cols]; rows];
+    for i in 1..rows {
+        for j in 1..cols {
             grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
         }
     }
-
-    grid[r - 1][c - 1]
+    grid[rows - 1][cols - 1]
 }
