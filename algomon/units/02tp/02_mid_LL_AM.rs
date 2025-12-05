3f7bda5e4d04c4f3c2d5f1f2ca75f7fd3fe0bac6
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-12-05 12:35:08 -0800

    adds annotation to TP02

--- algomon/units/02tp/02_mid_LL_AM.rs
+++ algomon/units/02tp/02_mid_LL_AM.rs
@@ -10,15 +10,14 @@ struct Node<T> {
     next: List<T>,
 }
 
+//will not handle None inputs?
 fn middle_of_linked_list(head: List<i32>) -> i32 {
     let mut slow = &head;
-
     let mut fast = &head;
 
     while let Some(curr) = fast {
         if let Some(next) = &curr.next {
             fast = &next.next;
-
             slow = &slow.as_ref().unwrap().next;
         } else {
             break;
